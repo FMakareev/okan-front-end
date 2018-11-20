a02a284aba3616969725c4fcdd8be7ea3eec5540
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-11-20 23:10:54 +0300

    Change proptypes

--- src/components/ButtonWithImage/ButtonWithImage.js
+++ src/components/ButtonWithImage/ButtonWithImage.js
@@ -1,37 +1,48 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Flex from '../Flex/Flex';
 import ButtonBase from '../ButtonBase/ButtonBase';
 
-class ButtonWithImage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    const { leftIcon, rightIcon, name, variant, size, mr, ml } = this.props;
-    return (
-      <ButtonBase variant={variant} size={size}>
-        <Flex justifyContent={'center'} alignItems={'center'}>
-          {leftIcon && (
-            <Flex mr={mr} justifyContent={'center'} alignItems={'center'}>
-              {leftIcon}
-            </Flex>
-          )}
-
-          <Flex justifyContent={'center'} alignItems={'center'}>
-            {name}
+export const ButtonWithImage = ({ leftIcon, rightIcon, name, variant, size, mr, ml }) => {
+  return (
+    <ButtonBase variant={variant} size={size}>
+      <Flex justifyContent={'center'} alignItems={'center'}>
+        {leftIcon && (
+          <Flex mr={mr} justifyContent={'center'} alignItems={'center'}>
+            {leftIcon}
           </Flex>
+        )}
 
-          {rightIcon && (
-            <Flex ml={ml} justifyContent={'center'} alignItems={'center'}>
-              {rightIcon}
-            </Flex>
-          )}
+        <Flex justifyContent={'center'} alignItems={'center'}>
+          {name}
         </Flex>
-      </ButtonBase>
-    );
-  }
-}
+
+        {rightIcon && (
+          <Flex ml={ml} justifyContent={'center'} alignItems={'center'}>
+            {rightIcon}
+          </Flex>
+        )}
+      </Flex>
+    </ButtonBase>
+  );
+};
+
+ButtonWithImage.PropTypes = {
+  /**name button */
+  name: PropTypes.string,
+  /**left icon */
+  leftIcon: PropTypes.string,
+  /**right icon */
+  rightIcon: PropTypes.string,
+  /**variant button */
+  variant: PropTypes.string,
+  /**size button */
+  size: PropTypes.string,
+  /**css value - margin-right */
+  mr: PropTypes.string,
+  /**css value - margin-left */
+  ml: PropTypes.string,
+};
 
 export default ButtonWithImage;
