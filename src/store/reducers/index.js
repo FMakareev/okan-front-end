35cdd6c56a12ca321cabff410e2715d32ec8ce01
NikolaZh <djozefs@gmail.com>
2018-11-20 19:46:51 +0300

    fix LayoutBase

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import { localeReducer as locale } from 'react-localize-redux';
+import { localizeReducer } from 'react-localize-redux';
 
 export const rootReducer = combineReducers({
   form: formReducer,
-  locale,
+  // locale: localizeReducer,
 });
 
 export default rootReducer;
