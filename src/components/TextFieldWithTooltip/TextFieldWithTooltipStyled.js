293defc474b688eeaaa5166a24906a11b3b75488
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-11-21 16:55:12 +0300

    Added style for text field tooltip

---
+++ src/components/TextFieldWithTooltip/TextFieldWithTooltipStyled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import { space } from 'styled-system';
+
+import TextField from '../TextField/TextField';
+
+export const Wrapper = styled.label`
+  ${space};
+  display: block;
+  position: relative;
+`;
+
+export const LabelStyled = styled.span`
+  position: absolute;
+  top: ${props => props.theme.space[3]}px;
+  left: ${props => props.theme.space[0]}px;
+  font-size: ${props => props.theme.fontSizes[4]}px;
+  line-height: ${props => props.theme.space[5]}px;
+  color: ${props => props.theme.colors.fontColor.color18};
+  font-weight: 500;
+  transform-origin: 0 0;
+  transition: all 0.2s ease;
+  cursor: text;
+`;
