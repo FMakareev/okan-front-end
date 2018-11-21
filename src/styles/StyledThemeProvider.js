8bf574f415175f208b06745b05746ff564f2b498
NikolaZh <djozefs@gmail.com>
2018-11-22 01:08:38 +0300

    connect fonts

--- src/styles/StyledThemeProvider.js
+++ src/styles/StyledThemeProvider.js
@@ -14,9 +14,12 @@ export const ColorPallet = {
   color6: '#006699',
   color7: '#00649C',
   color8: '#007CC4',
+  color9: '#F97E63',
 };
 
 const Space = [0, 2, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80];
+
+const Weight = [300, 500, 700];
 
 const Breakpoints = ['1024px', '1200px', '1440px'];
 
@@ -30,6 +33,7 @@ export const StyledThemeProvider = ({ children }) => (
     theme={{
       space: Space,
       fontSizes: Space,
+      fontWeight: Weight,
       lineHeight: Space,
       boxShadow,
       borderRadius: Space,
@@ -38,7 +42,7 @@ export const StyledThemeProvider = ({ children }) => (
       colors: ColorPallet,
       variant: { buttons: ButtonVariant },
       variants: { buttonSize: ButtonSize },
-      fontFamily: { regular: 'Roboto-Regular', bold: 'Roboto-Bold', medium: 'Roboto-Medium' },
+      fontFamily: { main: 'Museo Sans Cyrl', header: 'Circe Regular', headerBold: 'Circe Bold' },
     }}>
     {children}
   </Provider>
