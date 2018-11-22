24b103a316813de0c2bb7084c8c28459fc707874
NikolaZh <djozefs@gmail.com>
2018-11-23 01:37:34 +0300

    header create

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import styled from 'styled-components';
+import {
+  Wrapper,
+  LineWrapper,
+  LeftLineWrapper,
+  LogoWrapper,
+  ControlsWrapper,
+  Title,
+  ProfileButton,
+  ControlImg,
+  BurgerMenuButton,
+} from './HeaderStyled';
 
-// import PropTypes from 'prop-types';
+import Logo from '../../assets/icons/monocolor/headerLogo.monocolor.svg';
+import Play from '../../assets/icons/monocolor/play.monocolor.svg';
 
 export class Header extends Component {
-  static propTypes = {};
+  static propTypes = {
+    /** route name */
+    name: PropTypes.string,
+    /** open menu function */
+    onOpenMenu: PropTypes.func,
+  };
 
-  static defaultProps = {};
+  static defaultProps = {
+    name: 'Title not found',
+    onOpenMenu: null,
+  };
 
   constructor(props) {
     super(props);
@@ -19,8 +39,23 @@ export class Header extends Component {
   }
 
   render() {
-    console.log(this.props);
-    return <div>Header</div>;
+    const { name, onOpenMenu } = this.props;
+    return (
+      <Wrapper>
+        <LeftLineWrapper />
+        <LogoWrapper src={Logo} alt="logo" />
+        <LineWrapper />
+        <ControlsWrapper>
+          <Title>{name}</Title>
+          <ProfileButton>
+            <ControlImg src={Play} alt="profile" />
+          </ProfileButton>
+          <BurgerMenuButton onClick={onOpenMenu}>
+            <ControlImg src={Play} alt="profile" />
+          </BurgerMenuButton>
+        </ControlsWrapper>
+      </Wrapper>
+    );
   }
 }
 
