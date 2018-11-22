dad6fa629103dca7d5341c027c09893eee5943f0
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-11-22 15:48:21 +0300

    Change import

--- src/components/SmallPreloader/SmallPreloader.test.js
+++ src/components/SmallPreloader/SmallPreloader.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { StyledThemeProvider } from '../../styles/StyledThemeProvider';
-import { SmallPreloader } from './SmallPreloader';
+import SmallPreloader from './SmallPreloader';
 
 it('SmallPreloader: Рендерится без ошибок', () => {
   renderer.create(
