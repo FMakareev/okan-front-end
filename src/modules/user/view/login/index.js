f2e69f7f1bec54f71c8933f55950cbc5c9c72669
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-11-23 00:53:35 +0300

    Change stucture

--- src/modules/user/view/login/index.js
+++ src/modules/user/view/login/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+import FormLogin from '../../component/FormLogin/FormLogin';
+import ErrorCatch from '../../../../components/ErrorCatch/ErrorCatch';
+import Container from '../../../../components/Container/Container';
 
-export const LoginPage =() => (<div>
-  LoginPage
-</div>) ;
+class Login extends Component {
+  static propTypes = {};
 
-export default LoginPage;
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+    this.state = this.initialState;
+  }
+
+  get initialState() {
+    return {};
+  }
+
+  render() {
+    return (
+      <ErrorCatch>
+        <Container maxWidth={'500px'}>
+          <FormLogin />
+        </Container>
+      </ErrorCatch>
+    );
+  }
+}
+
+export default Login;
