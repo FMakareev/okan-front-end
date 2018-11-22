97b72df8818ddb7c731519f5a591b74691b83104
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-11-23 00:52:39 +0300

    Added test for Link

---
+++ src/components/Link/Link.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { StyledThemeProvider } from '../../styles/StyledThemeProvider';
+import { Link } from './Link';
+
+it('Link: Рендерится без ошибок', () => {
+  renderer.create(
+    <StyledThemeProvider>
+      <Link />
+    </StyledThemeProvider>,
+  );
+});
