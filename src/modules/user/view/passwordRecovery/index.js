099a8dd2ed557ee5e93443329f50f48ec6ad4d5f
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-11-23 14:06:11 +0300

    Create index file for password recovery

--- src/modules/user/view/passwordRecovery/index.js
+++ src/modules/user/view/passwordRecovery/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+import FormPasswordRecovery from '../../component/FormPasswordRecovery/FormPasswordRecovery';
+import ErrorCatch from '../../../../components/ErrorCatch/ErrorCatch';
+import Container from '../../../../components/Container/Container';
 
-export const PasswordRecoveryPage =() => (<div>
-  PasswordRecoveryPage
-</div>) ;
+class PasswordRecovery extends Component {
+  static propTypes = {};
 
-export default PasswordRecoveryPage;
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+    this.state = this.initialState;
+  }
+
+  get initialState() {
+    return {};
+  }
+
+  render() {
+    return (
+      <ErrorCatch>
+        <Container maxWidth={'500px'}>
+          <FormPasswordRecovery />
+        </Container>
+      </ErrorCatch>
+    );
+  }
+}
+
+export default PasswordRecovery;
