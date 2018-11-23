053cbb03a4901c9997a276b0f04fa24ac7b454e1
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-11-23 14:06:42 +0300

    Create index file for regdistration

--- src/modules/user/view/registration/index.js
+++ src/modules/user/view/registration/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+import FormRegistatration from '../../component/FormRegistatration/FormRegistatration';
+import ErrorCatch from '../../../../components/ErrorCatch/ErrorCatch';
+import Container from '../../../../components/Container/Container';
 
-export const RegistrationPage =() => (<div>
-  RegistrationPage
-</div>) ;
+class Login extends Component {
+  static propTypes = {};
 
-export default RegistrationPage;
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+    this.state = this.initialState;
+  }
+
+  get initialState() {
+    return {};
+  }
+
+  render() {
+    return (
+      <ErrorCatch>
+        <Container maxWidth={'500px'}>
+          <FormRegistatration />
+        </Container>
+      </ErrorCatch>
+    );
+  }
+}
+
+export default Login;
