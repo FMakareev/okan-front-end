de57792524a28bc64dc8cd06dedb242d862321e9
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-11-26 21:53:01 +0300

    Create component forn button

---
+++ src/modules/user/component/FormButton/FormButton.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Flex from '../../../../components/Flex/Flex';
+import ButtonWithImage from '../../../../components/ButtonWithImage/ButtonWithImage';
+
+import { SvgPlay } from '../../../../components/Icons/SvgPlay';
+
+export const FormButton = ({ disabled, children }) => {
+  return (
+    <Flex justifyContent={'center'}>
+        <ButtonWithImage
+          type="submit"
+          disabled={disabled}
+          variant={'large'}
+          size={'large'}
+          children={children}
+          ml={9}
+          rightIcon={SvgPlay()}
+        />
+    </Flex>
+  );
+};
+
+export default FormButton;
