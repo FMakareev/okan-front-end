2c5a94d03995fbd7cf6a74256f0b1212f5d9ebe1
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-11-26 21:51:49 +0300

    Create component Field input passwrod

---
+++ src/modules/user/component/FieldInputPassword/FieldInputPassword.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Field } from 'redux-form';
+import { Absolute } from 'rebass';
+
+import Flex from '../../../../components/Flex/Flex';
+import TextFieldBase from '../../../../components/TextFieldBase/TextFieldBase';
+import Relative from '../../../../components/Relative/Relative';
+import ButtonBase from '../../../../components/ButtonBase/ButtonBase';
+
+import required from '../../../../utils/validation/required';
+
+import { SvgEye } from '../../../../components/Icons/SvgEye';
+
+class FieldInputPassword extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { type: 'password', isOpen: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ isOpen: !this.state.isOpen, type: this.state.isOpen ? 'password' : 'text' });
+  }
+
+  render() {
+    const { type, isOpen } = this.state;
+    const { name, placeholder, validate } = this.props;
+
+    return (
+      <Flex width={'100%'}>
+        <Relative width={'100%'}>
+          <Field
+            name={name}
+            component={TextFieldBase}
+            placeholder={placeholder}
+            type={type}
+            validate={validate}
+          />
+          <Absolute top={'33%'} right={'4%'}>
+            <ButtonBase variant={'empty'} onClick={this.handleClick} borderRadius={5}>
+              {isOpen ? (
+                <div style={{ fill: '#00649C' }}>{SvgEye()}</div>
+              ) : (
+                <div style={{ fill: '#848484' }}> {SvgEye()}</div>
+              )}
+            </ButtonBase>
+          </Absolute>
+        </Relative>
+      </Flex>
+    );
+  }
+}
+
+export default FieldInputPassword;
