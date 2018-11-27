1f9cad60e36a432f484891e0249659e1a4577256
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-11-27 16:15:59 +0300

    Added reducers file

---
+++ src/store/reducers/user/reducers.js
@@ -0,0 +1,29 @@
+import { USER_ADD, USER_REMOVE } from './actionTypes';
+
+const initialState = null;
+
+export const ReducerUser = (prevState = initialState, { type, payload }) => {
+  switch (type) {
+    case USER_ADD:
+      return Object.assign({}, prevState, payload);
+    case USER_REMOVE:
+      if (isBrowser) {
+        window.localStorage.clear();
+
+        const cookies = document.cookie.split(';');
+
+        for (let i = 0; i < cookies.length; i += 1) {
+          const cookie = cookies[i];
+          const eqPos = cookie.indexOf('=');
+          const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+          document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+        }
+      }
+
+      return null;
+    default:
+      return prevState;
+  }
+};
+
+export default ReducerUser;
