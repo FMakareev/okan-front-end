33161f12da74b74d4348548b96891a04f75f6715
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-11-27 19:59:12 +0300

    Change structure

--- src/modules/user/view/profile/index.js
+++ src/modules/user/view/profile/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
+import ErrorCatch from '../../../../components/ErrorCatch/ErrorCatch';
+import Flex from '../../../../components/Flex/Flex';
+import Box from '../../../../components/Box/Box';
 
-export const ProfilePage = () => (<div>
-  ProfilePage
-</div>);
+import ProfileApproval from '../../component/ProfileApproval/ProfileApproval';
+import ProfileCreateUser from '../../component/ProfileCreateUser/ProfileCreateUser';
+
+export class ProfilePage extends Component {
+  state = {};
+  render() {
+    return (
+      <ErrorCatch>
+        <Flex ml={'240px'} mr={'70px'} justifyContent={'space-between'}>
+          <Flex flexDirection={'column'} width={'44%'}>
+            <ProfileApproval />
+            <div>1</div>
+          </Flex>
+
+          <Flex flexDirection={'column'} width={'44%'}>
+            <ProfileCreateUser />
+
+            <div>4</div>
+          </Flex>
+        </Flex>
+      </ErrorCatch>
+    );
+  }
+}
 
 export default ProfilePage;
