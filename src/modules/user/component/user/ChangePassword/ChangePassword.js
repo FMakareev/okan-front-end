1205821fe8d052a4bf5ac2d20365c8dab5672f67
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-11-28 21:57:24 +0300

    Create component change password

---
+++ src/modules/user/component/user/ChangePassword/ChangePassword.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Link from '../../../../../components/Link/Link';
+import ButtonWithImage from '../../../../../components/ButtonWithImage/ButtonWithImage';
+import { SvgPlay } from '../../../../../components/Icons/SvgPlay';
+
+export const ChangePassword = () => {
+  return (
+    <Link to={'/auth/password-recovery'}>
+      <ButtonWithImage
+        type="submit"
+        variant={'large'}
+        size={'medium'}
+        children={'Сменить пароль'}
+        rightIcon={SvgPlay()}
+        ml={9}
+        width={'100%'}
+      />
+    </Link>
+  );
+};
+
+export default ChangePassword;
