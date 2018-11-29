5cd9da1c095e001f7f2539734235efb6aff95497
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-11-29 21:40:10 +0300

    Create inde compoennt for revision file

--- src/modules/project/view/revisionList/index.js
+++ src/modules/project/view/revisionList/index.js
@@ -1,9 +1,35 @@
-// listProjectRevisions
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+/**PropTypes */
+import { ReactRoutePropTypes } from '../../../../propTypes/ReactRoutePropTypes';
 
-export const RevisionListPage = () => (<div>
-  RevisionListPage
-</div>) ;
+/**Components */
+import ErrorCatch from '../../../../components/ErrorCatch/ErrorCatch';
+import Flex from '../../../../components/Flex/Flex';
+import Container from '../../../../components/Container/Container';
+
+/** View */
+import RevisionList from '../component/RevisionList/RevisionList';
+
+class RevisionListPage extends Component {
+  static propTypes = { ...ReactRoutePropTypes };
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <ErrorCatch>
+        <Flex mt={9} justifyContent={'center'}>
+          <Container maxWidth={'800px'} width={'100%'}>
+            <RevisionList />
+          </Container>
+        </Flex>
+      </ErrorCatch>
+    );
+  }
+}
 
 export default RevisionListPage;
