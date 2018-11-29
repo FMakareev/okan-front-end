514698a33f5c7587f92527ea3504c2f5d7ec0f07
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-11-29 15:30:07 +0300

    Create component Product Project list

--- src/modules/project/view/projectList/index.js
+++ src/modules/project/view/projectList/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
+/**PropTypes */
+import { ReactRoutePropTypes } from '../../../../propTypes/ReactRoutePropTypes';
 
-export const ProjectListPage = () => (<div>
-  ProjectListPage
-</div>) ;
+/**Components */
+import ErrorCatch from '../../../../components/ErrorCatch/ErrorCatch';
+import Flex from '../../../../components/Flex/Flex';
+import Container from '../../../../components/Container/Container';
+
+/** View */
+import ProjectList from '../component/ProjectList';
+
+class ProjectListPage extends Component {
+  static propTypes = { ...ReactRoutePropTypes };
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <ErrorCatch>
+        <Flex mt={9} justifyContent={'center'}>
+          <Container maxWidth={'500px'} width={'100%'}>
+            <ProjectList />
+          </Container>
+        </Flex>
+      </ErrorCatch>
+    );
+  }
+}
 
 export default ProjectListPage;
