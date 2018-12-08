2241724b916399c37de0c8eb88fe1d43b900e6a3
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-12-08 15:18:06 +0300

    Create component title apge

---
+++ src/modules/project/component/DocumentSettings/TitlePage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Field, reduxForm, SubmissionError, Form } from 'redux-form';
+
+/**View */
+import Flex from '../../../../components/Flex/Flex';
+import ButtonWithImage from '../../../../components/ButtonWithImage/ButtonWithImage';
+import Text from '../../../../components/Text/Text';
+import Box from '../../../../components/Box/Box';
+import RichTextEditor from '../../../../components/RichTextEditor/RichTextEditor';
+
+export class TitlePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <Box>
+        <Text fz={6} lh={7} color={'color7'} textAlign={'center'} mb={6} fontFamily={'primary500'}>
+          Титульный лист
+        </Text>
+
+        <Flex width={'400px'}>
+          <Field name="documentSetting10" component={RichTextEditor} type="text" />
+        </Flex>
+      </Box>
+    );
+  }
+}
+
+TitlePage = reduxForm({
+  form: 'TitlePage',
+})(TitlePage);
+
+export default TitlePage;
