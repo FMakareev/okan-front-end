da5d5efc0b116d53c9760d59112b5c3b4ce620b7
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-12-08 15:19:17 +0300

    Create index file got Documents settings page

--- src/modules/project/view/documentSettings/index.js
+++ src/modules/project/view/documentSettings/index.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+/**PropTypes */
+import { ReactRoutePropTypes } from '../../../../propTypes/ReactRoutePropTypes';
 
-export const DocumentSettingsPage = () => (<div>
-  DocumentSettingsPage
-</div>);
+/** View */
+import ErrorCatch from '../../../../components/ErrorCatch/ErrorCatch';
+import Flex from '../../../../components/Flex/Flex';
+import Container from '../../../../components/Container/Container';
+
+/** Components */
+import DocumentSettings from '../../component/DocumentSettings/DocumentSettings';
+import TitlePage from '../../component/DocumentSettings/TitlePage';
+
+class DocumentSettingsPage extends Component {
+  static propTypes = { ...ReactRoutePropTypes };
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <ErrorCatch>
+        <Flex mt={9} justifyContent={'space-around'}>
+          <Container maxWidth={'500px'} width={'100%'}>
+            <DocumentSettings />
+          </Container>
+
+          <Container maxWidth={'400px'} width={'100%'}>
+            <TitlePage />
+          </Container>
+        </Flex>
+      </ErrorCatch>
+    );
+  }
+}
 
 export default DocumentSettingsPage;
