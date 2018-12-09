5171d4e9a39e74dfbb6df3cc506a0fe5398f3ed8
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-12-09 14:13:57 +0300

    Create component project Editor

---
+++ src/modules/project/component/ProjectEditor/ProjectEditor.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Field, reduxForm, SubmissionError, Form } from 'redux-form';
+
+/** Components */
+
+import ProjectEditorSideBar from './ProjectEditorSideBar';
+
+/**View */
+import Box from '../../../../components/Box/Box';
+
+/**PropTypes */
+import { formPropTypes } from '../../../../propTypes/Forms/FormPropTypes';
+
+export class ProjectEditor extends Component {
+  static propTypes = { ...formPropTypes };
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.submit = this.submit.bind(this);
+  }
+
+  submit(value) {}
+
+  render() {
+    const { handleSubmit, pristine, submitting, invalid } = this.props;
+
+    return (
+      <Form onSubmit={handleSubmit(this.submit)}>
+        <ProjectEditorSideBar />
+      </Form>
+    );
+  }
+}
+
+ProjectEditor = reduxForm({
+  form: 'ProjectEditor',
+})(ProjectEditor);
+
+export default ProjectEditor;
