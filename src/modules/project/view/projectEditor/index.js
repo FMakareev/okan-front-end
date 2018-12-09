6314b66815e99f6d833c7b4f5ac51897bd1ec22c
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-12-09 14:15:58 +0300

    Create component project Editor Page

--- src/modules/project/view/projectEditor/index.js
+++ src/modules/project/view/projectEditor/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+/**PropTypes */
+import { ReactRoutePropTypes } from '../../../../propTypes/ReactRoutePropTypes';
 
-export const ProjectEditorPage = () => (<div>
-  ProjectEditorPage
-</div>);
+/** View */
+import ErrorCatch from '../../../../components/ErrorCatch/ErrorCatch';
+
+/** Components */
+import ProjectEditor from '../../component/ProjectEditor/ProjectEditor';
+
+export class ProjectEditorPage extends Component {
+  static propTypes = { ...ReactRoutePropTypes };
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <ErrorCatch>
+        <ProjectEditor />
+      </ErrorCatch>
+    );
+  }
+}
 
 export default ProjectEditorPage;
