7b2e1efd4bb24fca935a3884c783fdfc85a86bcd
FMakareev <f.makareev@compaero.ru>
2018-12-17 02:57:07 +0300

    Переделал поиск и рендеринг маршрутов, добавил прелоадер

--- src/components/LayoutBase/LayoutBase.js
+++ src/components/LayoutBase/LayoutBase.js
@@ -1,5 +1,10 @@
 import React, { Fragment, PureComponent } from 'react';
+import { connect } from 'react-redux';
+
 import { LAYOUT_ADMIN, LAYOUT_APP, LAYOUT_AUTH } from '../../shared/layout';
+import {getUserFromStore} from "../../store/reducers/user/selectors";
+import {Text} from "../Text/Text";
+import {PreloaderWrapper, SpeedingWheel} from "../SmallPreloader/SmallPreloader";
 
 export class LayoutBase extends PureComponent {
   static propTypes = {};
@@ -7,27 +12,28 @@ export class LayoutBase extends PureComponent {
   constructor(props) {
     super(props);
     this.state = this.initialState;
+    // listen
   }
 
   get initialState() {
-    return {
-      Layout: null,
-      routes: null,
-      loading: true,
-      indexRoute: null,
-    };
-  }
-
-  componentDidMount() {
     const {
       route: { routes },
       location,
     } = this.props;
-    this.updateLayout(location, routes);
+    const indexRoute = this.findLayoutInPathname(location.pathname);
+
+    return {
+      indexRoute,
+      pathname: location.pathname,
+      Layout: routes[indexRoute] ? routes[indexRoute].component : null,
+      routes: routes[indexRoute],
+      loading: false,
+    };
   }
 
   componentWillReceiveProps(nextProps) {
-    const pathname = { ...this.state };
+    const pathname = this.state;
+
     if (nextProps.location.pathname !== pathname) {
       const {
         route: { routes },
@@ -38,16 +44,13 @@ export class LayoutBase extends PureComponent {
   }
 
   findLayoutInPathname = pathname => {
-    if (pathname.indexOf(`${LAYOUT_AUTH}`) === 1) {
-      return 0;
-    }
     if (pathname.indexOf(`${LAYOUT_APP}`) === 1) {
       return 1;
-    }
-    if (pathname.indexOf(`${LAYOUT_ADMIN}`) === 1) {
+    } else if (pathname.indexOf(`${LAYOUT_ADMIN}`) === 1) {
       return 2;
+    } else {
+      return 0;
     }
-    return undefined;
   };
 
   updateLayout = (location, routes) => {
@@ -64,11 +67,22 @@ export class LayoutBase extends PureComponent {
       () => console.log(`Run: layout - (${indexRoute}), path - ${location.pathname} `),
     );
   };
-
   render() {
     const { Layout, routes } = this.state;
-    return <Fragment>{Layout && <Layout {...this.props} route={routes} />}</Fragment>;
+    const { user } = this.props;
+    return <Fragment>
+      {user && user.initLoading && (
+        <PreloaderWrapper>
+          <Text fontSize={12}>
+            <SpeedingWheel />
+          </Text>
+        </PreloaderWrapper>
+      )}
+      {user && !user.initLoading && Layout && <Layout {...this.props} route={routes} />}
+    </Fragment>;
   }
 }
-
+LayoutBase = connect(state => ({
+  user: getUserFromStore(state),
+}))(LayoutBase);
 export default LayoutBase;
