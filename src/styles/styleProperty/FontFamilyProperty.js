aca2659a06c995f81cac7f27cb1bc0721cf51324
FMakareev <f.makareev@compaero.ru>
2018-12-17 02:53:05 +0300

    Переписал метод на styled-system

--- src/styles/styleProperty/FontFamilyProperty.js
+++ src/styles/styleProperty/FontFamilyProperty.js
@@ -1,14 +1,20 @@
+import { style } from 'styled-system';
+
 /**
  * @params {string} fontFamily - string name of the font name property in the theme
  * @params {object} theme
  * */
-export const FontFamilyProperty = ({ fontFamily, theme }) => {
-  try {
-    if (fontFamily && theme.fontFamily) {
-      return `font-family: '${theme.fontFamily[fontFamily]}';`;
-    }
-  } catch (e) {
-    console.error(e);
-  }
-};
+
+export const FontFamilyProperty = style({
+  // React prop name
+  prop: 'fontFamily',
+  // The corresponding CSS property (defaults to prop argument)
+  cssProperty: 'font-family',
+  // key for theme values
+  key: 'fontFamily',
+  // convert number values to pixels
+  numberToPx: false,
+  // shorthand alias React prop name
+  alias: 'fontFamily',
+});
 export default FontFamilyProperty;
