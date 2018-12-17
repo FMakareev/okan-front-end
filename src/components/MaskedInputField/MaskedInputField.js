0193a900e89b1303b85f1425cab31cc638c4b50c
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-12-17 22:42:25 +0300

    Create component masked input field

---
+++ src/components/MaskedInputField/MaskedInputField.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+/** View */
+import MaskedInput from '../MaskedInput/MaskedInput';
+import TooltipBase from '../TooltipBase/TooltipBase';
+
+/**PropTypes */
+import { fieldInputPropTypes } from '../../propTypes/Forms/FormPropTypes';
+import { fieldMetaPropTypes } from '../../propTypes/Forms/FormPropTypes';
+
+export class MaskedInputField extends Component {
+  static propTypes = {
+    placeholder: PropTypes.string,
+    type: PropTypes.string,
+    lineHeight: PropTypes.string,
+    fontFamily: PropTypes.string,
+    fontSize: PropTypes.string,
+    mask: PropTypes.string,
+    name: PropTypes.bool,
+    ...fieldInputPropTypes,
+    ...fieldMetaPropTypes,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const {
+      input,
+      name,
+      type,
+      meta,
+      placeholder,
+      required,
+      disabled,
+      loading,
+      fontSize,
+      lineHeight,
+      fontFamily,
+    } = this.props;
+
+    return (
+      <TooltipBase isActive={meta.touched && meta.error} warning={meta.error}>
+        <MaskedInput {...this.props} />
+      </TooltipBase>
+    );
+  }
+}
+
+export default MaskedInputField;
