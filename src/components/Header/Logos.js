54e038c0997a1a86054f2c526925a3d4e0936b85
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-12-18 17:34:26 +0300

    Change color inherit -> white

--- src/components/Header/Logos.js
+++ src/components/Header/Logos.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ProfileLogo = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="#fff" xmlns="http://www.w3.org/2000/svg">
     <mask id="mask0" maskType="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
       <circle cx="12" cy="12" r="12" fill="inherit" />
     </mask>
@@ -13,7 +13,7 @@ const ProfileLogo = () => (
 );
 
 const ProjectListLogo = () => (
-  <svg width="24" height="16" viewBox="0 0 24 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="24" height="16" viewBox="0 0 24 16" fill="#fff" xmlns="http://www.w3.org/2000/svg">
     <circle cx="2" cy="2" r="2" fill="inherit" />
     <rect x="6" y="1" width="18" height="2" rx="1" fill="inherit" />
     <circle cx="2" cy="8" r="2" fill="inherit" />
