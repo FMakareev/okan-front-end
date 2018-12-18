876c6ec0e5df399ed20b587fa9860f5d44bd92f9
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-12-18 17:32:43 +0300

    Change css property

--- src/components/ButtonBase/ButtonBase.js
+++ src/components/ButtonBase/ButtonBase.js
@@ -28,7 +28,7 @@ export const ButtonBase = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  border-radius: 5px;
+  ${props => BorderRadiusProperty({ ...props, borderRadius: '5px' })};
   ${buttonsVariant};
   ${buttonsSize};
   ${space};
