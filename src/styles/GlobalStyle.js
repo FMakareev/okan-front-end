74e9374c23b32490adfb3dc03064bfcf8c95ddf6
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-12-18 17:39:09 +0300

    Change import fonts

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,9 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  *{
+  * {
     box-sizing: border-box;
   }
+  
   body, html, #app {
     padding: 0;
     margin: 0;
@@ -14,54 +15,53 @@ export const GlobalStyle = createGlobalStyle`
 
   @font-face {
     font-family: 'Circe Regular';
-    src: url('../assets/fonts/Circe-Regular/Circe-Regular.eot');
-    src: url('../assets/fonts/Circe-Regular/Circe-Regular.eot?#iefix') format('embedded-opentype'),
-    url('../assets/fonts/Circe-Regular/Circe-Regular.woff') format('woff'),
-    url('../assets/fonts/Circe-Regular/Circe-Regular.ttf') format('truetype');
+    src: url('/assets/fonts/Circe-Regular/Circe-Regular.eot');
+    src: url('/assets/fonts/Circe-Regular/Circe-Regular.eot?#iefix') format('embedded-opentype'),
+      url('/assets/fonts/Circe-Regular/Circe-Regular.woff') format('woff'),
+      url('/assets/fonts/Circe-Regular/Circe-Regular.ttf') format('truetype');
     font-weight: normal;
     font-style: normal;
   }
 
   @font-face {
-  font-family: 'Circe Bold';
-  src: url('../assets/fonts/Circe-Bold/Circe-Bold.eot');
-  src: url('../assets/fonts/Circe-Bold/Circe-Bold.eot?#iefix') format('embedded-opentype'),
-  url('../assets/fonts/Circe-Bold/Circe-Bold.woff') format('woff'),
-  url('../assets/fonts/Circe-Bold/Circe-Bold.ttf') format('truetype');
-  font-weight: bold;
-  font-style: normal;
+    font-family: 'Circe Bold';
+    src: url('/assets/fonts/Circe-Bold/Circe-Bold.eot');
+    src: url('/assets/fonts/Circe-Bold/Circe-Bold.eot?#iefix') format('embedded-opentype'),
+      url('/assets/fonts/Circe-Bold/Circe-Bold.woff') format('woff'),
+      url('/assets/fonts/Circe-Bold/Circe-Bold.ttf') format('truetype');
+    font-weight: bold;
+    font-style: normal;
   }
 
   @font-face {
-  font-family: 'Museo Sans 300';
-  src: url('../assets/fonts/Museo/MuseoSansCyrl-300.eot');
-  src: url('../assets/fonts/Museo/MuseoSansCyrl-300.eot?#iefix') format('embedded-opentype'),
-  url('../assets/fonts/Museo/MuseoSansCyrl-300.woff') format('woff'),
-  url('../assets/fonts/Museo/MuseoSansCyrl-300.ttf') format('truetype');
-  font-weight: normal;
-  font-style: normal;
-}
+    font-family: 'Museo Sans 300';
+    src: url('/assets/fonts/Museo/MuseoSansCyrl-300.eot');
+    src: url('/assets/fonts/Museo/MuseoSansCyrl-300.eot?#iefix') format('embedded-opentype'),
+      url('/assets/fonts/Museo/MuseoSansCyrl-300.woff') format('woff'),
+      url('/assets/fonts/Museo/MuseoSansCyrl-300.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
 
- @font-face {
-  font-family: 'Museo Sans 500';
-  src: url('../assets/fonts/Museo/MuseoSansCyrl-500.eot');
-  src: url('../assets/fonts/Museo/MuseoSansCyrl-500.eot?#iefix') format('embedded-opentype'),
-  url('../assets/fonts/Museo/MuseoSansCyrl-500.woff') format('woff'),
-  url('../assets/fonts/Museo/MuseoSansCyrl-500.ttf') format('truetype');
-  font-weight: normal;
-  font-style: normal;
-}
+  @font-face {
+    font-family: 'Museo Sans 500';
+    src: url('/assets/fonts/Museo/MuseoSansCyrl-500.eot');
+    src: url('/assets/fonts/Museo/MuseoSansCyrl-500.eot?#iefix') format('embedded-opentype'),
+      url('/assets/fonts/Museo/MuseoSansCyrl-500.woff') format('woff'),
+      url('/assets/fonts/Museo/MuseoSansCyrl-500.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
 
- @font-face {
-  font-family: 'Museo Sans 700';
-  src: url('../assets/fonts/Museo/MuseoSansCyrl-700.eot');
-  src: url('../assets/fonts/Museo/MuseoSansCyrl-700.eot?#iefix') format('embedded-opentype'),
-  url('../assets/fonts/Museo/MuseoSansCyrl-700.woff') format('woff'),
-  url('../assets/fonts/Museo/MuseoSansCyrl-700.ttf') format('truetype');
-  font-weight: normal;
-  font-style: normal;
-}
-
+  @font-face {
+    font-family: 'Museo Sans 700';
+    src: url('/assets/fonts/Museo/MuseoSansCyrl-700.eot');
+    src: url('/assets/fonts/Museo/MuseoSansCyrl-700.eot?#iefix') format('embedded-opentype'),
+      url('/assets/fonts/Museo/MuseoSansCyrl-700.woff') format('woff'),
+      url('/assets/fonts/Museo/MuseoSansCyrl-700.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
 `;
 
 export default GlobalStyle;
