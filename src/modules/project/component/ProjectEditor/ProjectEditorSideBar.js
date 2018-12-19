4131814572bb9821344923cf53df514f9d6d02fa
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-12-19 14:34:20 +0300

    Change file

--- src/modules/project/component/ProjectEditor/ProjectEditorSideBar.js
+++ src/modules/project/component/ProjectEditor/ProjectEditorSideBar.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
-import SortableTree from 'react-sortable-tree';
-import 'react-sortable-tree/style.css'; // This only needs to be imported once in your app
 
 export default class ProjectEditorSideBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      treeData: [{ title: 'Chicken', children: [{ title: 'Egg' }] }],
-    };
+    this.state = {};
   }
 
   render() {
-    return (
-      <div style={{ height: 400 }}>
-        <SortableTree
-          treeData={this.state.treeData}
-          onChange={treeData => this.setState({ treeData })}
-        />
-      </div>
-    );
+    return <div>1</div>;
   }
 }
