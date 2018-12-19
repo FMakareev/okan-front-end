47f76cd27bbf0d136ee506d650affe8fbd7698d8
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-12-19 20:12:20 +0300

    Change structure

--- src/components/Link/Link.test.js
+++ src/components/Link/Link.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
 import { StyledThemeProvider } from '../../styles/StyledThemeProvider';
 import { Link } from './Link';
 
 it('Link: Рендерится без ошибок', () => {
   renderer.create(
     <StyledThemeProvider>
-      <Link to={'/'} />
+      <BrowserRouter>
+        <Link to="/" />
+      </BrowserRouter>
     </StyledThemeProvider>,
   );
 });
