912cbc20cdb69a5c35e4a0ef067aef53fd960989
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-12-19 20:13:02 +0300

    Change

--- src/modules/user/component/admin/ProfileNotification/ProfileNotification.js
+++ src/modules/user/component/admin/ProfileNotification/ProfileNotification.js
@@ -34,17 +34,17 @@ export class ProfileNotification extends Component {
           lineHeight={8}
           color={'color7'}
           textAlign={'center'}
-          mb={4}
+          mb={[4]}
           fontFamily={'primary500'}>
           Оповещения
         </Text>
 
-        <TextStyled fontSize={6} lineHeight={8} color={'color11'} mb={4} fontFamily={'secondary'}>
+        <TextStyled fontSize={6} lineHeight={8} color={'color11'} mb={[4]} fontFamily={'secondary'}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Ut enim ad minim
         </TextStyled>
 
-        <TextStyled fontSize={6} lineHeight={8} color={'color11'} mb={4} fontFamily={'secondary'}>
+        <TextStyled fontSize={6} lineHeight={8} color={'color11'} mb={[4]} fontFamily={'secondary'}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Ut enim ad minim
         </TextStyled>
