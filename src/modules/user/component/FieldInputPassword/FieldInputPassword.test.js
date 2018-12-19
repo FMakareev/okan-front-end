9adb58817395c5cb8016015b8796c9bc84e7b011
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-12-19 20:12:34 +0300

    Change structure

--- src/modules/user/component/FieldInputPassword/FieldInputPassword.test.js
+++ src/modules/user/component/FieldInputPassword/FieldInputPassword.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { StyledThemeProvider } from '../../../../styles/StyledThemeProvider';
-import { FieldInputPassword } from './FieldInputPassword';
+import FieldInputPassword from './FieldInputPassword';
 
 it('FieldInputPassword: Рендерится без ошибок', () => {
   renderer.create(
     <StyledThemeProvider>
-      <FieldInputPassword type={'password'} isOpen={true} name={'bu'} placeholder={'bu'} />
+      <FieldInputPassword name={'bu'} placeholder={'bu'} />
     </StyledThemeProvider>,
   );
 });
