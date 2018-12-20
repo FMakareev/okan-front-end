834443ffac609c17981169c63ba5f46915324401
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-12-20 21:24:45 +0300

    Change structure

--- src/store/reducers/user/actionTypes.js
+++ src/store/reducers/user/actionTypes.js
@@ -1,7 +1,16 @@
-export const USER_LOADING = 'USER_LOADING';
-export const USER_LOADING_ERROR = 'USER_LOADING_ERROR';
-export const USER_LOADING_SUCCESS = 'USER_LOADING_SUCCESS';
+/** Экшены инициализации пользователя в приложении */
+export const USER_INIT_LOADING_START = '@@user/USER_INIT_LOADING_START';
+export const USER_INIT_LOADING_ERROR = '@@user/USER_INIT_LOADING_ERROR';
+export const USER_INIT_LOADING_SUCCESS = '@@user/USER_INIT_LOADING_SUCCESS';
 
-export const USER_ADD = 'USER_ADD';
+/** Экшены обновления пользователя в приложении */
+export const USER_UPDATE_LOADING_START = '@@user/USER_UPDATE_LOADING_START';
+export const USER_UPDATE_LOADING_ERROR = '@@user/USER_UPDATE_LOADING_ERROR';
+export const USER_UPDATE_LOADING_SUCCESS = '@@user/USER_UPDATE_LOADING_SUCCESS';
 
-export const USER_REMOVE = 'USER_REMOVE';
+export const USER_ADD = '@@user/USER_ADD';
+
+export const USER_REMOVE = '@@user/USER_REMOVE';
+
+
+export const USER_NOT_AUTHORIZED = "User not authorized";
