d10d5d1d4ea38c8a37cb556124c0377f50347a51
Plasteev Mikhail <m.plasteev@compaero.ru>
2018-12-26 23:55:40 +0300

    Create Project EditorSideBar - Not working why it is not accepted by the internal styles

--- src/modules/project/component/ProjectEditor/ProjectEditorSideBar.js
+++ src/modules/project/component/ProjectEditor/ProjectEditorSideBar.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Treebeard } from 'react-treebeard';
 
-export default class ProjectEditorSideBar extends Component {
+const data = {
+  name: 'root',
+  toggled: true,
+  children: [
+    {
+      name: 'parent',
+      children: [{ name: 'child1' }, { name: 'child2' }],
+    },
+    {
+      name: 'loading parent',
+      loading: true,
+      children: [],
+    },
+    {
+      name: 'parent',
+      children: [
+        {
+          name: 'nested parent',
+          children: [{ name: 'nested child 1' }, { name: 'nested child 2' }],
+        },
+      ],
+    },
+  ],
+};
+
+const styles = {};
+
+class ProjectEditorSideBar extends Component {
   constructor(props) {
     super(props);
-
     this.state = {};
+    // this.onToggle = this.onToggle.bind(this);
   }
-
+  // onToggle(node, toggled) {
+  //   if (this.state.cursor) {
+  //     this.state.cursor.active = false;
+  //   }
+  //   node.active = true;
+  //   if (node.children) {
+  //     node.toggled = toggled;
+  //   }
+  //   this.setState({ cursor: node });
+  // }
   render() {
+    // return <Treebeard data={data} onToggle={this.onToggle} style={styles} />;
     return <div>1</div>;
   }
 }
+
+export default ProjectEditorSideBar;
