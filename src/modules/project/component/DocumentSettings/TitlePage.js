61d61fc5c89c18c8c887d2456b552f5c57aff451
Plasteev Mikhail <m.plasteev@compaero.ru>
2019-01-02 17:59:35 +0300

    Change structure

--- src/modules/project/component/DocumentSettings/TitlePage.js
+++ src/modules/project/component/DocumentSettings/TitlePage.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { Field, reduxForm, SubmissionError, Form } from 'redux-form';
+import { Field } from 'redux-form';
 
 /**View */
 import Flex from '../../../../components/Flex/Flex';
@@ -9,28 +8,20 @@ import Text from '../../../../components/Text/Text';
 import Box from '../../../../components/Box/Box';
 import RichTextEditor from '../../../../components/RichTextEditor/RichTextEditor';
 
-export class TitlePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <Box>
-        <Text fz={6} lh={7} color={'color7'} textAlign={'center'} mb={6} fontFamily={'primary500'}>
-          Титульный лист
-        </Text>
+export const TitlePage = () => {
+  return (
+    <Box>
+      <Text fz={6} lh={7} color={'color7'} textAlign={'center'} mb={6} fontFamily={'primary500'}>
+        Титульный лист
+      </Text>
 
-        <Flex width={'400px'}>
-          <Field name="documentSetting10" component={RichTextEditor} type="text" />
-        </Flex>
-      </Box>
-    );
-  }
-}
+      <Flex width={'400px'}>
+        <Field name="documentSetting10" component={RichTextEditor} type="text" />
+      </Flex>
+    </Box>
+  );
+};
 
-TitlePage = reduxForm({
-  form: 'TitlePage',
-})(TitlePage);
+TitlePage.propTypes = {};
 
 export default TitlePage;
