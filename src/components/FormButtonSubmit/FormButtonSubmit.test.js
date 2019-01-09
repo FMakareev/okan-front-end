eacc44710af35134f0a38bd9cccf6052f5f23b90
Plasteev Mikhail <m.plasteev@compaero.ru>
2019-01-10 00:50:26 +0300

    Rename

---
+++ src/components/FormButtonSubmit/FormButtonSubmit.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { StyledThemeProvider } from '../../../../styles/StyledThemeProvider';
+import { FormButton } from './FormButton';
+
+it('FormButton: Рендерится без ошибок', () => {
+  renderer.create(
+    <StyledThemeProvider>
+      <FormButton />
+    </StyledThemeProvider>,
+  );
+});
+
+it('FormButton: Рендерится без ошибок с свойствами', () => {
+  renderer.create(
+    <StyledThemeProvider>
+      <FormButton children={'button'} disabled={false} ml={9} />
+    </StyledThemeProvider>,
+  );
+});
