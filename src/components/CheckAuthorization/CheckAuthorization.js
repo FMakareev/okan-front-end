667eb5422073baa1a78119c220229d7e6a360a03
Plasteev Mikhail <m.plasteev@compaero.ru>
2019-01-10 17:56:55 +0300

    Fix bugs

--- src/components/CheckAuthorization/CheckAuthorization.js
+++ src/components/CheckAuthorization/CheckAuthorization.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-// TODO: доделать редирект на страницу с ошибкой доступа или просто поставить компонент
+
 /**
  * @param {array} role - массив ролей у которых есть доступ к компоненту
  * @param {object} AccessDeniedCallback - компонент кторый должен быть отображен в случае ошибки
  * @desc
  * */
-export const CheckAuthorization = (role = [], AccessDeniedCallback = null) => WrappedComponent => {
-  console.log(1111, role, AccessDeniedCallback, WrappedComponent);
+export let CheckAuthorization = (role = [], AccessDeniedCallback = null) => WrappedComponent => {
   return props => {
     try {
       const { user } = props;
       if (!user || (user && user.error)) {
-        return <Redirect to={'/registration'} />;
+        return <Redirect to={'/'} />;
       }
       if (user && user.initLoading) {
         return null;
