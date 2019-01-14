b25b2f67a5ce9192d417f3213ce28c7fb3207c99
Plasteev Mikhail <m.plasteev@compaero.ru>
2019-01-15 00:41:25 +0300

    Change component

--- src/components/Icons/SvgSidebarSettings.js
+++ src/components/Icons/SvgSidebarSettings.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 export const SvgSidebarSettings = () => {
   return (
-    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="inherit"
+      xmlns="http://www.w3.org/2000/svg">
       <rect
         x="0.25"
         y="0.25"
