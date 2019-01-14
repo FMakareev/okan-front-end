21c6616930ebdaa8f14d46502ec4b4392679e4d9
Plasteev Mikhail <m.plasteev@compaero.ru>
2019-01-15 00:41:48 +0300

    Change import

--- src/apollo/graphql/query/userlist.js
+++ src/apollo/graphql/query/userlist.js
@@ -1,4 +1,4 @@
 import { range } from '../../helpers/range';
-import { useritem } from './userItem';
+import { useritem } from './useritem';
 
 export const userlist = (query, props) => range(100).map(d => useritem());
