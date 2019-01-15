53ea47a1aff471948ee960f482dab4f79c742ac6
fmakareev <fmakareev@code-artel.com>
2019-01-15 11:56:41 +0300

    sync

--- src/modules/project/component/ProjectEditor/ProjectEditor.js
+++ src/modules/project/component/ProjectEditor/ProjectEditor.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { Field, reduxForm, SubmissionError, Form } from 'redux-form';
 
 /** Components */
 
@@ -11,24 +8,15 @@ import ProjectEditorSideBar from './ProjectEditorSideBar';
 import Box from '../../../../components/Box/Box';
 
 /**PropTypes */
-import { formPropTypes } from '../../../../propTypes/Forms/FormPropTypes';
 import {Flex} from "@lib/ui/Flex/Flex";
 import { DragDropContext } from 'react-dnd'
 
 import HTML5Backend from 'react-dnd-html5-backend'
 
-// ProjectEditorSideBarDragDropContext = DragDropContext(HTML5Backend)(ProjectEditorSideBar);
 
-let defaultManager;
-function getDefaultManager() {
-  if (!defaultManager) {
-    defaultManager = new DragDropManager(HTML5Backend);
-  }
-  return defaultManager;
-}
 
 export class ProjectEditor extends Component {
-  static propTypes = { ...formPropTypes };
+  static propTypes = {  };
 
 
 
