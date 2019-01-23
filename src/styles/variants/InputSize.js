cae9408db886baaa1dd161fd3fc2b69f75f11cb8
fmakareev <fmakareev@code-artel.com>
2019-01-23 08:29:13 +0300

    Create inputSize variant

---
+++ src/styles/variants/InputSize.js
@@ -0,0 +1,22 @@
+export const InputSize = {
+  'xs':{
+    padding: '0 10px',
+    fontSize: '16px',
+    lineHeight: '18px',
+  },
+  'sm': {
+    padding: '4px 10px',
+    fontSize: '16px',
+    lineHeight: '20px',
+  },
+  'md': {
+    padding: '10px 10px',
+    fontSize: '16px',
+    lineHeight: '20px',
+  },
+  'lg': {
+    padding: '10px 10px',
+    fontSize: '28px',
+    lineHeight: '36px',
+  },
+};
