ffee07b4f7ddc9b2a88b0e740e0bfe7ff1e79bf8
fmakareev <fmakareev@code-artel.com>
2019-01-24 10:18:19 +0300

    Add data

--- src/modules/project/component/ProjectList/ProjectList.js
+++ src/modules/project/component/ProjectList/ProjectList.js
@@ -9,9 +9,6 @@ import Box from '../../../../components/Box/Box';
 import ButtonWithImage from '../../../../components/ButtonWithImage/ButtonWithImage';
 import Link from '../../../../components/Link/Link';
 
-/** Image */
-import settings from '../../../../assets/image/settings.png';
-import { SvgPlay } from '../../../../components/Icons/SvgPlay';
 
 export class ProjectList extends Component {
   static propTypes = {};
@@ -22,36 +19,13 @@ export class ProjectList extends Component {
   }
 
   render() {
+    const {data} = this.props;
     return (
       <Box>
-        <ProjectItem />
+        {
+          data && data.map((item, index) => (<ProjectItem {...item} key={`ProjectItem-${index}`}/>))
+        }
 
-        <ProjectItem />
-
-        <ProjectItem />
-
-        <ProjectItem />
-
-        <ProjectItem />
-
-        <ProjectItem />
-
-        <ProjectItem />
-
-        <ProjectItem />
-
-        <Link mr={6} to={`/app/project-create`} textDecoration={'none'}>
-          <ButtonWithImage
-            type="submit"
-            variant={'large'}
-            size={'medium'}
-            children={'Создать проект'}
-            rightIcon={SvgPlay()}
-            ml={9}
-            width={'100%'}
-            widthIcon={'10px'}
-          />
-        </Link>
       </Box>
     );
   }
