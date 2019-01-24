653dc41b1ae946c9b116172ad538bf58af57036d
Plasteev Mikhail <m.plasteev@compaero.ru>
2019-01-24 23:44:40 +0300

    Delete  file

--- src/apollo/index.client.js
+++ src/apollo/index.client.js
@@ -1,22 +1,28 @@
 /* global ENDPOINT_CLIENT */
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import fetch from 'unfetch';
+// import { ApolloClient } from 'apollo-client';
+// import { createHttpLink } from 'apollo-link-http';
+// import { InMemoryCache } from 'apollo-cache-inmemory';
+// import fetch from 'unfetch';
+
+// import mocksClient from './mocksClient';
+
+// export const client = () => {
+//   // return mocksClient;
+//   return new ApolloClient({
+//     cache: new InMemoryCache().restore(window.APOLLO_STATE),
+//     link: createHttpLink({
+//       uri: `${ENDPOINT_CLIENT}/graphql`,
+//       credentials: 'same-origin',
+//       fetch,
+//     }),
+//     ssrForceFetchDelay: 100,
+//   });
+// };
+
+// export default client;
 
 import mocksClient from './mocksClient';
 
-export const client = () => {
-  // return mocksClient;
-  return new ApolloClient({
-    cache: new InMemoryCache().restore(window.APOLLO_STATE),
-    link: createHttpLink({
-      uri: `${ENDPOINT_CLIENT}/graphql`,
-      credentials: 'same-origin',
-      fetch,
-    }),
-    ssrForceFetchDelay: 100,
-  });
-};
+export const client = mocksClient;
 
 export default client;
