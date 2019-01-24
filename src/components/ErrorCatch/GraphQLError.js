09d5562ecab67f64377e4d5d38c8494e6ecd55a2
Plasteev Mikhail <m.plasteev@compaero.ru>
2019-01-24 23:46:48 +0300

    Change strucut're

--- src/components/ErrorCatch/GraphQLError.js
+++ src/components/ErrorCatch/GraphQLError.js
@@ -1,38 +1,36 @@
 import React from 'react';
 
-export const GraphQLError = (error, translate) => {
+export const GraphQLError = error => {
   switch (error.message) {
     case 'GraphQL error: User not logged': {
       return {
-        message: translate('error_user_not_logged'),
+        message: 'error_user_not_logged',
         redirect: '/logout',
       };
     }
     case 'GraphQL error: User session not found': {
       return {
-        message: translate('error_user_session_not_found_title'),
+        message: 'error_user_session_not_found_title',
         redirect: '/logout',
       };
     }
     case 'GraphQL error: You not accessed': {
       return {
-        title: translate('error_access_denied_title'),
-        message: translate('error_access_denied_message'),
+        title: 'error_access_denied_title',
+        message: 'error_access_denied_message',
       };
     }
     case 'GraphQL error: not found': {
-      return {
-        title: translate('error_page_not_found_title'),
-        message: (
-          <span>
-            {error.params.name} {translate('error_page_not_found_message')}
-          </span>
-        ),
-      };
+      return { title: 'error_page_not_found_title', message: 'error_page_not_found_title' };
+      // (
+      //   // <span>
+      //   //   {error.params.name} {'error_page_not_found_message'}
+      //   // </span>
+      // ),
     }
     default:
       return {
-        title: translate('error_unexpected_error_title'),
+        title: 'error_unexpected_error_title',
       };
   }
 };
