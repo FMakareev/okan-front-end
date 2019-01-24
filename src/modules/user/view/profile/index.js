4cf4984d475449299978d50a2030254a52d1ff33
Plasteev Mikhail <m.plasteev@compaero.ru>
2019-01-24 11:19:42 +0300

    Added graphql schema and changed structure

--- src/modules/user/view/profile/index.js
+++ src/modules/user/view/profile/index.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Query, graphql, compose } from 'react-apollo';
 
 /**View*/
+import SmallPreloader from '../../../../components/SmallPreloader/SmallPreloader';
 import ErrorCatch from '../../../../components/ErrorCatch/ErrorCatch';
 import Flex from '../../../../components/Flex/Flex';
 import Box from '../../../../components/Box/Box';
@@ -26,6 +28,10 @@ import { ROLE_ADMIN, ROLE_USER } from '../../../../shared/roles';
 
 /** Redux user */
 import { getUserFromStore } from '../../../../store/reducers/user/selectors';
+
+/** Graphql schema */
+import UserItemQuery from './UserItemQuery.graphql';
+import UserListQuery from './UserListQuery.graphql';
 
 const LeftColumn = styled(Flex)`
   width: calc(100% - 400px);
@@ -50,10 +56,11 @@ export class ProfilePage extends Component {
 
   render() {
     const {
-      user: { role },
+      user: { role, id },
     } = this.props;
 
-    return <ErrorCatch>
+    return (
+      <ErrorCatch>
         <Flex ml={'10%'} mr={'70px'} mt={9} flexDirection={'column'}>
           <Flex justifyContent={'space-between'} mb={'100px'}>
             <LeftColumn flexDirection={'column'}>
@@ -61,24 +68,69 @@ export class ProfilePage extends Component {
             </LeftColumn>
 
             <RightColumn flexDirection={'column'}>
-              { ROLE_ADMIN && <FormProfileCreateUser />}
+              {ROLE_ADMIN && <FormProfileCreateUser />}
 
-              {role ===  ROLE_USER && <FormPersonData lastname={'Колесников'} firstname={'Александр'} patronymic={'Владиславович'} birthdate={'12.12.1984'} position={'Специалист по технической документации'} phone={'8-999-888-77-66'} email={'email@okan.su'} />}
+              {ROLE_USER && (
+                <Query query={UserListQuery}>
+                  {({ loading, error, data }) => {
+                    console.log('dataList', data);
+
+                    if (id && loading) {
+                      return <SmallPreloader />;
+                    }
+                    if (error) {
+                      throw error;
+                    }
+                    if (id && data && !data.userlist) {
+                      throw { message: `GraphQL error: not found` };
+                    }
+
+                    return (
+                      <Query query={UserItemQuery} variables={{ ...(id ? { id } : null) }}>
+                        {({ loading, error, data }) => {
+                          console.log('dataItem', data);
+                          if (id && loading) {
+                            return <SmallPreloader />;
+                          }
+                          if (error) {
+                            throw error;
+                          }
+                          if (id && data && !data.useritem) {
+                            throw { message: `GraphQL error: not found` };
+                          }
+                          return (
+                            <FormPersonData
+                              initialValues={data && Object.assign({}, { ...data.useritem })}
+                            />
+                          );
+                        }}
+                      </Query>
+                    );
+                  }}
+                </Query>
+              )}
             </RightColumn>
           </Flex>
 
           <Flex justifyContent={'space-between'}>
             <LeftColumn flexDirection={'column'}>
-              <FormProfileNotification data={{ id: '23415', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim' }} />
+              <FormProfileNotification
+                data={{
+                  id: '23415',
+                  message:
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim',
+                }}
+              />
             </LeftColumn>
 
             <RightColumn flexDirection={'column'}>
-              { role ===  ROLE_ADMIN && <FormProfileRecoveryEmail />}
-              { role ===  ROLE_USER && <FormChangePassword />}
+              {ROLE_ADMIN && <FormProfileRecoveryEmail />}
+              {role === ROLE_USER && <FormChangePassword />}
             </RightColumn>
           </Flex>
         </Flex>
-      </ErrorCatch>;
+      </ErrorCatch>
+    );
   }
 }
 
