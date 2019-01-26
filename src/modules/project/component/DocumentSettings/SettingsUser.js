9f5155fe1cde41b65a4b63c5d9fa93af6ed94c11
Plasteev Mikhail <m.plasteev@compaero.ru>
2019-01-26 20:14:11 +0300

    Cretae component settings user

---
+++ src/modules/project/component/DocumentSettings/SettingsUser.js
@@ -0,0 +1,55 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Field } from 'redux-form';
+
+/**View */
+import Flex from '../../../../components/Flex/Flex';
+import Box from '../../../../components/Box/Box';
+import Text from '../../../../components/Text/Text';
+import CheckboxBase from '../../../../components/CheckboxBase/CheckboxBase';
+
+/** Styles property */
+import BorderColorProperty from '../../../../styles/styleProperty/BorderColorProperty';
+
+const FlexStyled = styled(Flex)`
+  border-top: 1px solid;
+  ${props => BorderColorProperty({ ...props, borderColor: 'color7' })};
+  padding-bottom: 12px;
+`;
+
+export const SettingsUser = props => {
+  const { data } = props;
+  return (
+    <Fragment>
+      <Box mb={'100px'}>
+        <Text
+          fontSize={6}
+          lineHeight={8}
+          color={'color7'}
+          textAlign={'center'}
+          mb={6}
+          fontFamily={'primary500'}>
+          Участники проекта
+        </Text>
+
+        {data.map((item, index) => (
+          <FlexStyled pt={3}>
+            <Field name={`documentSetting${index}`} component={CheckboxBase} />
+
+            <Text fontFamily={'primary300'} fontSize={6} lineHeight={8} color={'color11'} ml={20}>
+              {item.firstname}
+            </Text>
+          </FlexStyled>
+        ))}
+      </Box>
+    </Fragment>
+  );
+};
+
+SettingsUser.propTypes = {
+  /** Data  */
+  data: PropTypes.element,
+};
+
+export default SettingsUser;
