9531a8244c3801f8bd5853dfc820a19e6de30657
Plasteev Mikhail <m.plasteev@compaero.ru>
2019-01-29 18:13:49 +0300

    Fix import

--- src/modules/user/component/FormProfileNotification/FormProfileNotification.js
+++ src/modules/user/component/FormProfileNotification/FormProfileNotification.js
@@ -1,35 +1,33 @@
 import React, { Fragment } from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 /**View */
-import Text from '../../../../components/Text/Text';
+import Text from '@lib/ui/Text/Text';
 
 /** Components */
 import FormProfileNotificationItem from '../FormProfileNotificationItem/FormProfileNotificationItem';
 
-export const FormProfileNotification = ({ initialValues}) => {
+export const FormProfileNotification = ({ initialValues }) => {
   return (
-      <Fragment>
-        <Text
-          fontSize={6}
-          lineHeight={8}
-          color={'color7'}
-          textAlign={'center'}
-          mb={[4]}
-          fontFamily={'primary500'}>
-          Оповещения
-        </Text>
+    <Fragment>
+      <Text
+        fontSize={6}
+        lineHeight={8}
+        color={'color7'}
+        textAlign={'center'}
+        mb={[4]}
+        fontFamily={'primary500'}>
+        Оповещения
+      </Text>
 
-        <FormProfileNotificationItem message={'1'} name={'Фамилия Имя'} time={'Время'} />
-      </Fragment>
-    );
-}
+      <FormProfileNotificationItem message={'1'} name={'Фамилия Имя'} time={'Время'} />
+    </Fragment>
+  );
+};
 
 FormProfileNotification.propTypes = {
-    /** message user*/
-    message: PropTypes.string,
-  };
-    
+  /** message user*/
+  message: PropTypes.string,
+};
 
 export default FormProfileNotification;
