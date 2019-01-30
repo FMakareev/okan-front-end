26f544aef0f16020184ecf3b02c2aae5becb48b9
fmakareev <fmakareev@code-artel.com>
2019-01-30 21:49:45 +0300

    Add map reviaionlist

--- src/modules/project/component/RevisionList/RevisionList.js
+++ src/modules/project/component/RevisionList/RevisionList.js
@@ -1,33 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import styled from 'styled-components';
-import { reduxForm, Form, getFormValues } from 'redux-form';
 
 /**View */
-import Td from '@lib/ui/Table/Td';
 import Th from '@lib/ui/Table/Th';
 import Tr from '@lib/ui/Table/Tr';
-import Flex from '@lib/ui/Flex/Flex';
 import Table from '@lib/ui/Table/Table';
 import Tbody from '@lib/ui/Table/Tbody';
-import ButtonBase from '@lib/ui/ButtonBase/ButtonBase';
+import {RevisionItem} from "../RevisionItem/RevisionItem";
 
-/** Image */
-import { SvgExport } from '@lib/ui/Icons/SvgExport';
-import { SvgFolder } from '@lib/ui/Icons/SvgFolder';
-
-/** Styles property */
-import { BorderColorProperty } from '../../../../styles/styleProperty/BorderColorProperty';
 
 const TableStyled = styled(Table)`
   border-collapse: collapse;
   table-layout: fixed;
-`;
-
-const TdStyled = styled(Td)`
-  border-top: 2px solid;
-  ${props => BorderColorProperty({ ...props, borderColor: 'color7' })}
 `;
 
 export class RevisionList extends Component {
@@ -36,8 +22,7 @@ export class RevisionList extends Component {
   state = {};
 
   render() {
-    const { initialValues } = this.props;
-
+    const {data} = this.props;
     return (
       <TableStyled width={'100%'}>
         <Tr>
@@ -58,49 +43,25 @@ export class RevisionList extends Component {
             fontFamily={'primary500'}
             color={'color11'}
             py={4}
-            width={'500px'}>
+            width={'auto'}>
             Ф. И. О.
           </Th>
 
-          <Th fontSize={6} lineHeight={8} fontFamily={'primary500'} color={'color11'} py={4}>
+          <Th width={'175px'} fontSize={6} lineHeight={8} fontFamily={'primary500'} color={'color11'} py={4}>
             Дата
           </Th>
 
-          <Th fontSize={6} lineHeight={8} fontFamily={'primary500'} color={'color11'} py={4} />
+          <Th width={'100px'} fontSize={6} lineHeight={8} fontFamily={'primary500'} color={'color11'} py={4}/>
         </Tr>
-        {/*initialValues && initialValues.map(item => console.log(item.name))*/}
         <Tbody>
-          <Tr>
-            <TdStyled fontFamily={'primary300'} py={4} pl={3}>
-              1{/*initialValues[0]*/}
-            </TdStyled>
-            <TdStyled fontFamily={'primary300'} py={4} textAlign={'center'}>
-              {'initialValues'}
-            </TdStyled>
-            <TdStyled fontFamily={'primary300'} py={4} textAlign={'center'}>
-              12.11.2018
-            </TdStyled>
-            <TdStyled fontFamily={'primary300'}>
-              <Flex justifyContent={'center'}>
-                <ButtonBase variant={'empty'}>{SvgFolder()}</ButtonBase>
+        {
+          data.map((item, index) => (<RevisionItem key={`RevisionItem-${index}`} {...item}/>))
+        }
 
-                <ButtonBase variant={'empty'}>{SvgExport()}</ButtonBase>
-              </Flex>
-            </TdStyled>
-          </Tr>
         </Tbody>
       </TableStyled>
     );
   }
 }
-
-RevisionList = connect(
-  state => ({ values: getFormValues('RevisionList')(state) }),
-  null,
-)(RevisionList);
-
-RevisionList = reduxForm({
-  form: 'RevisionList',
-})(RevisionList);
 
 export default RevisionList;
