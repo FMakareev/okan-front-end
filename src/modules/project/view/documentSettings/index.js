fe94dce3bf2e5786290c1f0749303dfdb0f70fac
Plasteev Mikhail <m.plasteev@compaero.ru>
2019-01-31 02:13:55 +0300

    Changed structured

--- src/modules/project/view/documentSettings/index.js
+++ src/modules/project/view/documentSettings/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Query } from 'react-apollo';
 
 /** Redux user */
 import { getUserFromStore } from '../../../../store/reducers/user/selectors';
@@ -12,7 +13,12 @@ import { ReactRoutePropTypes } from '../../../../propTypes/ReactRoutePropTypes';
 import ErrorCatch from '@lib/ui/ErrorCatch/ErrorCatch';
 
 /** Components */
-import DocumentSettings from '../../component/DocumentSettings/';
+import FormDocumentSettings from '../../component/FormDocumentSettings/FormDocumentSettings';
+
+/** Graphql schema */
+import DocumentItemQuery from './DocumentItemQuery.graphql';
+
+const has = Object.prototype.hasOwnProperty;
 
 class DocumentSettingsPage extends Component {
   static propTypes = { ...ReactRoutePropTypes };
@@ -21,16 +27,40 @@ class DocumentSettingsPage extends Component {
 
   render() {
     const {
-      match: {
-        params: { id },
-      },
-      route: { name },
-      location: { key },
+      match: { params },
     } = this.props;
 
     return (
       <ErrorCatch>
-        <DocumentSettings initialValues={{ id, name, key }} />
+        <Query query={DocumentItemQuery} variables={{ id: params.id }}>
+          {({ data, error, loading }) => {
+            // console.log('DocumentItemQuery', data, error, loading);
+
+            if (loading) {
+              console.error('loading:', loading);
+              return 'Загрузка ...';
+            }
+            if (error) {
+              console.error('Error:', error);
+              return 'Ошибка ...';
+            }
+
+            if (!data || (data && !has.call(data, 'documentitem'))) {
+              return null;
+            }
+
+            return (
+              <FormDocumentSettings
+                initialValues={{
+                  ...data.documentitem,
+                  partners: data.documentitem
+                    ? data.documentitem.partners.map(item => item.id)
+                    : [],
+                }}
+              />
+            );
+          }}
+        </Query>
       </ErrorCatch>
     );
   }
