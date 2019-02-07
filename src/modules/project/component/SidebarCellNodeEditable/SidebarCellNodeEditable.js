87e037e31d8a7341e980c668155f60de5e1e84dd
fmakareev <fmakareev@code-artel.com>
2019-02-07 16:19:48 +0300

    Стилизовал textarea добавив максимальную ширину

--- src/modules/project/component/SidebarCellNodeEditable/SidebarCellNodeEditable.js
+++ src/modules/project/component/SidebarCellNodeEditable/SidebarCellNodeEditable.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import ContentEditable from 'react-contenteditable';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Mutation } from 'react-apollo';
 import UpdateCellMutation from './UpdateCellMutation.graphql';
-import { Text } from '@lib/ui/Text/Text';
 
-const ContentEditableStyled = styled(ContentEditable)`
-  max-width: 90%;
-  ${({ disabled }) =>
-    disabled
-      ? ''
-      : `
-    border: 1px solid;
-    border-color: #2290ba;
-    border-radius: 4px;
-    box-shadow: 0 0 4px 0px rgba(0,127,175,1);
-  `}
-  ${props => console.log(111, props)}
+const TextareaStyled = styled.textarea`
+  max-width: 150px;
+  width: 100%;
+
 `;
 
 // TODO: добавить таймер для автоматического схранения данных и стэйт перенести сюда
@@ -33,7 +23,7 @@ export const SidebarCellNodeEditable = React.forwardRef(
             return html;
           } else {
             return (
-              <textarea
+              <TextareaStyled
                 ref={ref}
                 value={html}
                 autoFocus={focused}
