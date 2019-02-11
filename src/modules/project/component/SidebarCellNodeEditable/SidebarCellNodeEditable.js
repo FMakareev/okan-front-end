00c2ab9a464484385d6ede80d15f9ca186b5ab4f
fmakareev <fmakareev@code-artel.com>
2019-02-11 14:01:42 +0300

    Добавил обновление кеша

--- src/modules/project/component/SidebarCellNodeEditable/SidebarCellNodeEditable.js
+++ src/modules/project/component/SidebarCellNodeEditable/SidebarCellNodeEditable.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { Mutation } from 'react-apollo';
+import {Mutation} from 'react-apollo';
 import UpdateCellMutation from './UpdateCellMutation.graphql';
+import CellItemQuery from '../DocumentTree/CellItemQuery.graphql';
 
 const TextareaStyled = styled.textarea`
   max-width: 150px;
@@ -10,13 +11,36 @@ const TextareaStyled = styled.textarea`
 
 `;
 
+
+const UpdateCache = (store, {data: {updatecell}}) => {
+  try {
+    let options = {
+      query: CellItemQuery,
+      variables: {
+        id: updatecell.cell.id,
+      },
+    };
+
+    store.writeQuery({
+      ...options,
+      data: {
+        cellitem:updatecell.cell,
+      },
+    });
+  } catch (error) {
+    console.error('Error UpdateCache: ', error);
+  }
+};
+
+
 // TODO: добавить таймер для автоматического схранения данных и стэйт перенести сюда
 
 export const SidebarCellNodeEditable = React.forwardRef(
-  ({ html, focused, onChange, id, onToggle }, ref) => {
+  ({html, focused, onChange, id, onToggle}, ref) => {
     return (
-      <Mutation onError={() => {}} mutation={UpdateCellMutation}>
-        {(mutate, { called, data, error, loading }) => {
+      <Mutation onError={() => {
+      }} mutation={UpdateCellMutation}>
+        {(mutate, {called, data, error, loading}) => {
           // console.log(mutate, { called, data, error, loading });
 
           if (!focused) {
@@ -32,13 +56,13 @@ export const SidebarCellNodeEditable = React.forwardRef(
                   if (event.key === 'Enter') {
                     event.persist();
                     onToggle();
-                    mutate({ variables: { id, name: html } });
+                    mutate({variables: {id, name: html}, update: UpdateCache});
                   }
                 }}
                 onBlur={() => {
                   if (focused) {
                     onToggle();
-                    mutate({ variables: { id, name: html } });
+                    mutate({variables: {id, name: html}, update: UpdateCache});
                   }
                 }}
                 onClick={event => {
