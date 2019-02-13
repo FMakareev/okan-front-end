f20d9afb67fb1cf835f90173504fa99975a186d7
Ivan <ivanszhuravlev@gmail.com>
2019-02-14 01:31:46 +0300

    Remove plugin, add block type for heading

--- src/components/FroalaReduxForm/DefineIcons.js
+++ src/components/FroalaReduxForm/DefineIcons.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 import copyIcon from '../../assets/image/copyIcon.png';
 import bindIcon from '../../assets/image/bindIcon.png';
 import deleteIcon from '../../assets/image/deleteIcon.png';
+// import './NameInputPlugin';
 
 export function DefineIcons(props) {
   $.FroalaEditor.DefineIcon('copy', {
@@ -41,6 +42,51 @@ export function DefineIcons(props) {
       props.buttonClick(command);
     },
   });
+
+  // var publicMethod = require('./NameInputPlugin');
+
+  // Add an option for your plugin.
+  // $.FroalaEditor.DEFAULTS = $.extend($.FroalaEditor.DEFAULTS, {
+  //   myOption: false
+  // });
+
+  // // Define the plugin.
+  // // The editor parameter is the current instance.
+  // $.FroalaEditor.PLUGINS.myPlugin = function (editor) {
+  //   // Private variable visible only inside the plugin scope.
+  //   var private_var = 'My awesome plugin';
+
+  //   // Private method that is visible only inside plugin scope.
+  //   function _privateMethod () {
+  //     console.log (private_var);
+  //   }
+
+  //   // Public method that is visible in the instance scope.
+  //   function publicMethod () {
+  //     console.log (_privateMethod());
+  //   }
+
+  //   // The start point for your plugin.
+  //   function _init () {
+  //     // You can access any option from documentation or your custom options.
+  //     console.log(typeof editor)
+  //     editor.append('Some test text');
+
+  //     // Call any method from documentation.
+  //     // editor.methodName(params);
+
+  //     // You can listen to any event from documentation.
+  //     // editor.events.add('contentChanged', function (params) {});
+  //   }
+
+  //   // Expose public methods. If _init is not public then the plugin won't be initialized.
+  //   // Public method can be accessed through the editor API:
+  //   // $('.selector').froalaEditor('myPlugin.publicMethod');
+  //   return {
+  //     _init: _init,
+  //     publicMethod: publicMethod
+  //   }
+  // }
 
   return null;
 }
--- src/shared/blockType.js
+++ src/shared/blockType.js
@@ -1,9 +1,11 @@
 export const BLOCK_TABLE = 'table';
 export const BLOCK_IMAGE = 'image';
 export const BLOCK_TEXT  = 'text';
+export const BLOCK_HEADING = 'heading';
 
 export default {
   BLOCK_TABLE,
   BLOCK_IMAGE,
   BLOCK_TEXT,
+  BLOCK_HEADING,
 }
