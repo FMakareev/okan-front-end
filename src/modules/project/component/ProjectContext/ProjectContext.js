9bb678389ba68bb02a3d79dac0521fe6d342ec09
fmakareev <fmakareev@code-artel.com>
2019-02-15 19:47:59 +0300

    Добавлены ProjectContextPropTypes

--- src/modules/project/component/ProjectContext/ProjectContext.js
+++ src/modules/project/component/ProjectContext/ProjectContext.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const ProjectContext = React.createContext();
 
@@ -10,4 +11,26 @@ export const withProject = Component => {
       </ProjectContext.Consumer>
     );
   };
+};
+
+export const ProjectContextPropTypes = {
+  project: PropTypes.shape({
+    position: PropTypes.shape({
+      cellid: PropTypes.string,
+      sectionid: PropTypes.string,
+      documentid: PropTypes.string,
+      projectid: PropTypes.string,
+    }),
+    project: {
+      author: PropTypes.shape({
+        id: PropTypes.string,
+      }),
+      createdate: PropTypes.string,
+      documents: PropTypes.array,
+      id: PropTypes.string,
+      name: PropTypes.string,
+      status: PropTypes.string,
+      updatedate: PropTypes.string,
+    }
+  }),
 };
