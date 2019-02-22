db2fe35a0156c6010b0446b6d486dc8e9dd62ef5
Plasteev Mikhail <m.plasteev@compaero.ru>
2019-02-22 13:38:09 +0300

    fix

--- src/modules/project/component/ProjectSidebar/ProjectSidebar.js
+++ src/modules/project/component/ProjectSidebar/ProjectSidebar.js
@@ -1,19 +1,18 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import {Query} from 'react-apollo';
-import {Action, withStateMachine} from 'react-automata'
+import { Query } from 'react-apollo';
 
 /** View */
-import {DocumentTree} from '../DocumentTree/DocumentTree';
+import { DocumentTree } from '../DocumentTree/DocumentTree';
 import FormCreateDocument from '../FormCreateDocument/FormCreateDocument';
-import {PROJECT_MODE_RW, withProject} from '../ProjectContext/ProjectContext';
+import { PROJECT_MODE_RW, withProject } from '../ProjectContext/ProjectContext';
 
 /** Graphql Schema */
 import DocumentListQuery from './DocumentListQuery.graphql';
 
 /** PropTypes */
-import {ProjectPropTypes} from '../../../../propTypes/ProjectPropTypes';
-import ProjectModeState from "../ProjectContext/ProjectModeState";
+import { ProjectPropTypes } from '../../../../propTypes/ProjectPropTypes';
+import ProjectModeState from '../ProjectContext/ProjectModeState';
 
 const DocumentTreeWithProject = withProject(props => <DocumentTree {...props} />);
 const FormCreateDocumentWithProject = withProject(props => <FormCreateDocument {...props} />);
@@ -24,18 +23,18 @@ export class ProjectSidebar extends Component {
   };
 
   render() {
-    const {documents, id, project} = this.props;
+    const { documents, id, project } = this.props;
     return (
       <Fragment>
         {documents &&
-        documents.map((item) => (
-          <DocumentTreeWithProject data={item} key={`DocumentTree=${item.id}`}/>
-        ))}
+          documents.map(item => (
+            <DocumentTreeWithProject data={item} key={`DocumentTree=${item.id}`} />
+          ))}
 
-        {!documents && <DocumentTreeWithProject data={this.props} key={`DocumentTree=${id}`}/>}
+        {!documents && <DocumentTreeWithProject data={this.props} key={`DocumentTree=${id}`} />}
 
         <ProjectModeState is={PROJECT_MODE_RW}>
-          <FormCreateDocumentWithProject/>
+          <FormCreateDocumentWithProject />
         </ProjectModeState>
       </Fragment>
     );
