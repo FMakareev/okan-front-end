8c7f73101428251344ac698248df6455ad105f47
fmakareev <fmakareev@code-artel.com>
2019-02-23 19:18:16 +0300

    Remove unused import

--- src/modules/project/component/ProjectSidebar/ProjectSidebar.js
+++ src/modules/project/component/ProjectSidebar/ProjectSidebar.js
@@ -1,18 +1,13 @@
 import React, {Component, Fragment} from 'react';
-import PropTypes from 'prop-types';
-import {Query} from 'react-apollo';
-import {Action, withStateMachine} from 'react-automata'
+
 
 /** View */
 import {DocumentTree} from '../DocumentTree/DocumentTree';
 import FormCreateDocument from '../FormCreateDocument/FormCreateDocument';
 import {PROJECT_MODE_RW, withProject} from '../ProjectContext/ProjectContext';
 
-/** Graphql Schema */
-import DocumentListQuery from './DocumentListQuery.graphql';
 
 /** PropTypes */
-import {ProjectPropTypes} from '../../../../propTypes/ProjectPropTypes';
 import ProjectModeState from "../ProjectContext/ProjectModeState";
 
 const DocumentTreeWithProject = withProject(props => <DocumentTree {...props} />);
@@ -20,11 +15,10 @@ const FormCreateDocumentWithProject = withProject(props => <FormCreateDocument {
 
 export class ProjectSidebar extends Component {
   static propTypes = {
-    project: ProjectPropTypes,
   };
 
   render() {
-    const {documents, id, project} = this.props;
+    const {documents, id,} = this.props;
     return (
       <Fragment>
         {documents &&
