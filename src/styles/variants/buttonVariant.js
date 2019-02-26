00fb40ce9bedc833444fdd179388e4a869b797f3
fmakareev <fmakareev@code-artel.com>
2019-02-26 20:03:27 +0300

    sync

--- src/styles/variants/buttonVariant.js
+++ src/styles/variants/buttonVariant.js
@@ -4,21 +4,33 @@ export const ButtonVariant = {
     fill: '#fff',
     backgroundColor: '#00649C',
     border: '1px solid #00649C',
-    '&:hover': { backgroundColor: '#fff', color: '#00649C', fill: '#00649C' },
+    '&:hover': {
+      backgroundColor: '#fff',
+      color: '#00649C',
+      fill: '#00649C'
+    },
   },
   small: {
     color: '#848484',
     backgroundColor: '#fff',
     fill: '#848484',
     border: '1px solid',
-    '&:hover': { backgroundColor: '#848484', color: '#ffffff', fill: '#ffffff' },
+    '&:hover': {
+      backgroundColor: '#848484',
+      color: '#ffffff',
+      fill: '#ffffff'
+    },
   },
   large: {
     color: '#00649C',
     fill: '#00649C',
     backgroundColor: 'transparent',
     border: '2px solid',
-    '&:hover': { backgroundColor: '#00649C', color: '#ffffff', fill: '#ffffff' },
+    '&:hover': {
+      backgroundColor: '#00649C',
+      color: '#ffffff',
+      fill: '#ffffff'
+    },
   },
   outlineGray: {
     backgroundColor: '#ffffff',
@@ -26,15 +38,13 @@ export const ButtonVariant = {
     stroke: '#848484',
     border: '0.5px solid #848484',
     borderRadius: '5px',
-//     padding: 2px;
-// font-size: 15px;
     '&:hover': {
       color: '#00649C',
       fill: '#00649C',
       stroke: '#00649C',
       borderColor: '#00649C',
     },
-    ':disabled':{
+    ':disabled': {
       backgroundColor: '#e5e5e5',
       fill: '#e5e5e5',
     }
@@ -43,9 +53,13 @@ export const ButtonVariant = {
     backgroundColor: '#ffffff',
     fill: '#848484',
     stroke: '#848484',
-    border: 0 ,
-    '&:hover': { color: '#00649C', fill: '#00649C', stroke: '#00649C' },
-    ':disabled':{
+    border: 0,
+    '&:hover': {
+      color: '#00649C',
+      fill: '#00649C',
+      stroke: '#00649C'
+    },
+    ':disabled': {
       backgroundColor: '#e5e5e5',
       fill: '#e5e5e5',
     }
