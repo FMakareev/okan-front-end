6dce580d77de144e19e690258cc00c1a11fea01b
fmakareev <fmakareev@code-artel.com>
2019-02-27 01:18:04 +0300

    Переделал метод partnersList. Добавил комментариев.

--- src/modules/project/component/EditorCellCommentController/EditorCellCommentController.js
+++ src/modules/project/component/EditorCellCommentController/EditorCellCommentController.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { Absolute } from 'rebass';
-import { connect } from 'react-redux';
-import { Field, Form, reduxForm } from 'redux-form';
+import {Absolute} from 'rebass';
+import {connect} from 'react-redux';
+import {Field, Form, reduxForm} from 'redux-form';
 import styled from 'styled-components';
 
 /** Components */
 import EditorCellCommentButton from '../EditorCellCommentButton/EditorCellCommentButton';
-import { EditorCellCommentItem } from '../EditorCellCommentItem/EditorCellCommentItem';
+import {EditorCellCommentItem} from '../EditorCellCommentItem/EditorCellCommentItem';
 
 /** View */
 import Box from '@lib/ui/Box/Box';
-import { Relative } from '@lib/ui/Relative/Relative';
+import {Relative} from '@lib/ui/Relative/Relative';
 import TextAreaBase from '@lib/ui/TextAreaBase/TextAreaBase';
-import { Flex } from '@lib/ui/Flex/Flex';
+import {Flex} from '@lib/ui/Flex/Flex';
 
 /** Styles css */
 import BorderColorProperty from '@lib/styles/styleProperty/BorderColorProperty';
 import BackgroundColorProperty from '@lib/styles/styleProperty/BackgroundColorProperty';
 
 /** Reducer */
-import { getUserFromStore } from '../../../../store/reducers/user/selectors';
+import {getUserFromStore} from '../../../../store/reducers/user/selectors';
 
 const FormStyled = styled(Form)`
   width: 250px;
   border: 1px solid;
-  ${props => BorderColorProperty({ ...props, borderColor: 'color4' })};
-  ${props => BackgroundColorProperty({ ...props, backgroundColor: 'color0' })};
+  ${props => BorderColorProperty({...props, borderColor: 'color4'})};
+  ${props => BackgroundColorProperty({...props, backgroundColor: 'color0'})};
   border-bottom-left-radius: 5px;
   border-top-right-radius: 5px;
   border-top-left-radius: 5px;
@@ -36,10 +36,11 @@ class FormCommentEditor extends Component {
   render() {
     return (
       <Box zIndex={1} right={'10px'} top={'10px'}>
-        <FormStyled onSubmit={() => {}}>
-          <Field name={'message'} component={TextAreaBase} />
+        <FormStyled onSubmit={() => {
+        }}>
+          <Field name={'message'} component={TextAreaBase}/>
         </FormStyled>
-        <Flex justifyContent={'flex-end'} />
+        <Flex justifyContent={'flex-end'}/>
       </Box>
     );
   }
@@ -71,10 +72,12 @@ export class EditorCellCommentController extends Component {
     };
   }
 
-  /** @desc метод для получения статуса для кнопки комментария */
+  /**
+   * @return {string}
+   * @desc метод для получения статуса для кнопки комментария */
   getCurrentStatus = () => {
     try {
-      const { comment, user, id } = this.props;
+      const {comment, user, id} = this.props;
       // console.log('EditorCellCommentController: ', this.props);
       if (user && user.isAuth) {
         if (!comment) {
@@ -89,6 +92,7 @@ export class EditorCellCommentController extends Component {
       }
     } catch (error) {
       console.error(`Error getCurrentStatus id=${id}:`, error);
+      return 'emptyComment';
     }
   };
 
@@ -135,17 +139,24 @@ export class EditorCellCommentController extends Component {
     }
   };
 
-  /** @desc Получаем список комментов */
+  /**
+   * @return {array}
+   * @desc Получаем список комментов */
   get partnersList() {
-    const commentsList =
-      this.props.comments &&
-      this.props.comments.filter(item => item.sender.id === this.props.user.id);
-    return commentsList;
+    try {
+      if (Array.isArray(this.props.comments)) {
+        return this.props.comments.filter(item => item.sender.id === this.props.user.id);
+      }
+      return []
+    } catch (error) {
+      console.error('Error: ', error);
+      return []
+    }
   }
 
   render() {
-    const { isOpen, status } = this.state;
-    const { comments, project, user } = this.props;
+    const {isOpen, status} = this.state;
+    const {comments, project, user} = this.props;
 
     /** @desc скрываю кнопку комментариев для автора проекта если коментариев нет */
     if (
@@ -160,7 +171,7 @@ export class EditorCellCommentController extends Component {
     const comment = this.partnersList ? this.partnersList : null;
     return (
       <Relative>
-        <EditorCellCommentButton status={status} onClick={this.onClick} />
+        <EditorCellCommentButton status={status} onClick={this.onClick}/>
         {Array.isArray(comments) && comments.length > 0 && isOpen && (
           <Absolute zIndex={5} className={'EditorCellCommentWrapper'} top={'20px'} right={0}>
             <EditorCellCommentItem
