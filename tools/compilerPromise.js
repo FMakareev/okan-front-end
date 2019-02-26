d0e2b7012aa35dc809d5221d15fe350b521cefb1
fmakareev <fmakareev@code-artel.com>
2019-02-26 20:28:10 +0300

    sync

--- tools/compilerPromise.js
+++ tools/compilerPromise.js
@@ -5,6 +5,8 @@ export const compilerPromise = compiler =>
         return resolve();
       }
       console.log('stats.errors: ', stats.errors);
+      console.log('stats.compilation: ', stats.compilation);
+      console.log('stats.compilation.errors: ', stats.compilation.errors);
       // return Promise.reject(new Error('Compiler promise error.'));
     });
   });
