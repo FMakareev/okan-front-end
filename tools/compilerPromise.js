72770de25ceab42bec93fb0f1389d8ca46075156
fmakareev <fmakareev@code-artel.com>
2019-02-26 20:32:16 +0300

    sync

--- tools/compilerPromise.js
+++ tools/compilerPromise.js
@@ -4,9 +4,14 @@ export const compilerPromise = compiler =>
       if (!stats.hasErrors()) {
         return resolve();
       }
-      console.log('stats.errors: ', stats.errors);
-      console.log('stats.compilation.error: ', stats.compilation.error);
-      console.log('stats.compilation.errors: ', stats.compilation.errors);
+      // console.log('stats.errors: ', stats.errors);
+      // console.log('stats.compilation.error: ', stats.compilation.error);
+      // console.log('stats.compilation.errors: ', stats.compilation.errors);
+      if(Array.isArray(stats.compilation.errors)){
+        stats.compilation.errors.forEach(item=>{
+          console.error(item);
+        })
+      }
       // return Promise.reject(new Error('Compiler promise error.'));
     });
   });
