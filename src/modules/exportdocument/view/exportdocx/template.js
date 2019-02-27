855b5c8207f44a911a748f2e7dba87a69a9e7c02
fmakareev <fmakareev@code-artel.com>
2019-02-27 19:20:51 +0300

    sync

---
+++ src/modules/exportdocument/view/exportdocx/template.js
@@ -0,0 +1,63 @@
+export default `
+<table style="width: 100%; margin-left: calc(0%);">
+	<tbody>
+		<tr>
+			<td style="width: 50.1208%;">
+				<br>
+			</td>
+			<td style="width: 49.7584%;">
+				<div class="fr-text-uppercase"><strong><span style="font-size: 12px;">Утверждаю:</span></strong></div>
+				<div><strong><span style="font-size: 12px;"><br></span></strong><span style="font-size: 12px;">АО &quot;Концерн Росэнергоатом&quot;<br>Первый заместитель Генерального директора по эксплуатации АЭС _________________А.В. Шутиков<br>&laquo;___&raquo; __________2018 г.</span></div>
+				<br>
+				<div><span style="font-size: 12px;">АО &laquo;ОКАН&raquo;<br>Генеральный директор<br>_________________В.О. Томшинский<br>&laquo;___&raquo; __________2018 г.</span></div>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2" style="width: 99.8792%;">
+				<br>
+				<div style="text-align: center;"><span style="font-size: 18px;"><strong>ТЕХНИЧЕСКИЕ УСЛОВИЯ&nbsp;</strong></span></div>
+				<div style="text-align: center;"><strong><span style="font-size: 18px;">ТУ-ОК-Т-001-2018&nbsp;</span></strong></div>
+				<div style="text-align: center;"><span style="font-size: 18px;"><strong>&lt;НАИМЕНОВАНИЕ ОБОРУДОВАНИЯ&gt;</strong></span></div>
+
+				<p>
+					<br>
+				</p>
+			</td>
+		</tr>
+		<tr>
+			<td style="width: 50.1208%;">
+				<div class="fr-text-uppercase"><strong><span style="font-size: 12px;">Согласовано:</span></strong></div><span style="font-size: 12px;">АО &laquo;Концерн Росэнергоатом&raquo;<br>Заместитель генерального директора,<br>Директор по производству и <br>эксплуатации АЭС<br>_________________А.А. Дементьев<br>&laquo;___&raquo; __________2018 г.</span>
+
+				<p><span style="font-size: 12px;">АО &laquo;Концерн Росэнергоатом&raquo;<br>Директор по качеству<br>_________________В.Н. Блинков<br>&laquo;___&raquo; __________2018 г.</span></p><span style="font-size: 12px;">Филиал АО &laquo;Концерн Росэнергоатом&raquo;<br>Нововоронежская АЭС</span>
+				<br><span style="font-size: 12px;">Главный инженер</span>
+				<br><span style="font-size: 12px;">_________________А.И. Федоров</span>
+				<br><span style="font-size: 12px;">&laquo;___&raquo; __________2018 г.</span>
+				<br>
+				<br><span style="font-size: 12px;">АО &laquo;Атомэнергопроект&raquo;<br>Заместитель директора по&nbsp;</span>
+				<br><span style="font-size: 12px;">проектированию действующих АЭС<br>_________________В.А. Андрианов<br>&laquo;___&raquo; __________2018 г.</span>
+				<br>
+				<br>
+				<br>
+
+				<p>
+					<br>
+				</p>
+
+				<p>
+					<br>
+				</p>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+			</td>
+			<td style="width: 49.7584%; vertical-align: top;">
+				<div style="text-align: left;"><strong><span style="font-size: 12px;">РАЗРАБОТАЛ:</span></strong></div><span style="font-size: 12px;">АО &laquo;ОКАН&raquo;<br>Инженер проекта<br>_________________<br>&laquo;___&raquo; __________2018 г.</span>
+				<br>
+				<br>
+			</td>
+		</tr>
+	</tbody>
+</table>
+`
