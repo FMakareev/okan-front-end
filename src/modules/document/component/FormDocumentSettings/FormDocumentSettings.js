9ab523375104420ef4cba00dfe63035ea83586a2
fmakareev <fmakareev@code-artel.com>
2019-03-01 13:09:04 +0300

    Добавил в documentitem свойство equipmentname. Добавил запрос userlist

--- src/modules/document/component/FormDocumentSettings/FormDocumentSettings.js
+++ src/modules/document/component/FormDocumentSettings/FormDocumentSettings.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Query } from 'react-apollo';
-import { connect } from 'react-redux';
-import { graphql } from 'react-apollo';
-import styled from 'styled-components';
-import { success, error } from 'react-notification-system-redux';
-import { Fields, Field, reduxForm, SubmissionError, Form, getFormValues } from 'redux-form';
-import { Redirect } from 'react-router-dom';
-
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {graphql} from 'react-apollo';
+import {success, error} from 'react-notification-system-redux';
+import {Fields, reduxForm, SubmissionError, Form, getFormValues, FieldArray} from 'redux-form';
+import { withRouter} from 'react-router-dom';
+import {has} from '../../../../utils/has';
 /**PropTypes */
-import { ReactRoutePropTypes } from '../../../../propTypes/ReactRoutePropTypes';
+import {ReactRoutePropTypes} from '../../../../propTypes/ReactRoutePropTypes';
 
 /** View */
 import Box from '@lib/ui/Box/Box';
@@ -18,16 +15,18 @@ import Container from '@lib/ui/Container/Container';
 import ButtonWithImage from '@lib/ui/ButtonWithImage/ButtonWithImage';
 
 /**Image */
-import { SvgSave } from '@lib/ui/Icons/SvgSave';
+import {SvgSave} from '@lib/ui/Icons/SvgSave';
 
 /** Components */
-import SettingsUser from './SettingsUser';
-import SettingsNameDocument from './SettingsNameDocument';
+import BasicDocumentSettings from '../BasicDocumentSettings/BasicDocumentSettings';
 
 /** Graphql schema */
-import ProjectItemQuery from './ProjectItemQuery.graphql';
 import FormDocumentSettingsMutation from './FormDocumentSettingsMutation.graphql';
 import DocumentItemQuery from '../../view/documentSettings/DocumentItemQuery.graphql';
+import InnerApprovalPartnersQuery from "../InnerApprovalPartnersQuery/InnerApprovalPartnersQuery";
+import ContractorListField from "../ContractorListField/ContractorListField";
+import {Text} from "@lib/ui/Text/Text";
+import {ROLE_EXTERNALCONTRACTOR, ROLE_USER} from "@lib/shared/roles";
 
 const notificationOpts = () => ({
   success: {
@@ -44,65 +43,232 @@ const notificationOpts = () => ({
   },
 });
 
-const has = Object.prototype.hasOwnProperty;
-
-const sleep = ms =>
-  new Promise(resolve => {
-    return setInterval(resolve, ms);
-  });
 
 export class FormDocumentSettings extends Component {
-  static propTypes = { ...ReactRoutePropTypes };
+  static propTypes = {...ReactRoutePropTypes};
 
   state = this.initialState;
 
   get initialState() {
-    return { redirect: null };
+    return {redirect: null};
   }
 
-  submit = value => {
-    // console.log('FormDocumentSettings', value);
-    const data = {
-      variables: Object.assign({}, value),
+  /**
+   * @param {object} value
+   * @param {string} value.user.firstname
+   * @param {string} value.user.lastname
+   * @param {string} value.user.patronymic
+   * @param {string} value.user.organizationname
+   * @param {string} value.user.position
+   * @param {string} value.user.signature
+   * @param {string} value.user.role
+   * @param {string} value.approvaldate
+   * @return {object} возвращает структуру gql типа ContractorApproval
+   * @desc метод для преобразования данных к типу ContractorApproval
+   * */
+  createContractorApproval = (value) => {
+    try {
+      return {
+        id: value.user.id,
+        approvaldate: value.approvaldate,
+      }
+    } catch (error) {
+      console.error('Error createContractorApproval: ', error);
+      return null;
+    }
+  };
+
+  /**
+   * @param {object} value
+   * @param {string} value.firstname
+   * @param {string} value.lastname
+   * @param {string} value.patronymic
+   * @param {string} value.organizationname
+   * @param {string} value.position
+   * @param {string} value.approvaldate
+   * @param {string} value.signature
+   * @param {string} value.role
+   * @return {Promise}
+   * @desc метод для создания пользователя
+   * */
+  createUser = (value) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject({
+          message: 'ОШИБКА!!!!!!!!!!!'
+        })
+        // resolve({
+        //   createuser: {
+        //     user: {
+        //       ...value,
+        //       id: 'айдишник'
+        //     }
+        //   }
+        // })
+      }, 2000);
+    })
+  };
+
+  /**
+   * @param {object} value
+   * @param {string} value.id
+   * @param {string} value.firstname
+   * @param {string} value.lastname
+   * @param {string} value.patronymic
+   * @param {string} value.organizationname
+   * @param {string} value.position
+   * @param {string} value.approvaldate
+   * @param {string} value.signature
+   * @param {string} value.role
+   * @return {Promise}
+   * @desc метод для обновления пользователя
+   * */
+  updateUser = (value) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          updateuser: {
+            user: {
+              ...value,
+              id: 'айдишник'
+            }
+          }
+        })
+      }, 2000);
+    })
+  };
+
+
+  /**
+   * @param {array} value
+   * @param {object} value[i].user
+   * @param {string} value[i].approvaldate
+   * @return {Promise}
+   * @desc метод для преобразования данных из формы к типу ContractorApproval
+   * */
+  createContractorApprovalList = async (value) => {
+    try {
+      return await value.map(async (item) => {
+
+        if (item.user.role === ROLE_EXTERNALCONTRACTOR) { /** если это внешний контрагент */
+          if (has.call(item.user, 'id')) {
+            /** обновляем пользователя */
+            const {updateuser} = await this.updateUser(item.user);
+            return this.createContractorApproval({
+              ...item,
+              ...updateuser,
+            });
+          } else { /** пользователь системы */
+            /** создаем пользователя */
+            const {createuser} = await this.createUser(item.user);
+            return this.createContractorApproval({
+              ...item,
+              ...createuser,
+            });
+          }
+        } else if (item.user.role === ROLE_USER) {
+          /** если это внутренний пользователь системы */
+          return this.createContractorApproval(item);
+        }
+      })
+    } catch (error) {
+      console.error('Error createContractorApprovalList: ', error);
+    }
+  };
+
+  /**
+   * @param {object} value
+   * @param {array} value.externalapprove
+   * @param {array} value.externalconform
+   * @return {Promise}
+   * @desc метод для преобразования данные формы к виду массивов gql типов ContractorApproval
+   * */
+  transformDocumentApproval = async (value) => {
+    try {
+      const newDate = {
+        externalapprove: [],
+        externalconform: []
+      };
+      if (has.call(value, 'externalapprove') && Array.isArray(value.externalapprove)) {
+        newDate.externalapprove = await this.createContractorApprovalList(value.externalapprove)
+      }
+      if (has.call(value, 'externalconform') && Array.isArray(value.externalconform)) {
+        newDate.externalconform = await  this.createContractorApprovalList(value.externalconform)
+      }
+      console.log('newDate: ', newDate);
+
+      /** резолвим промисы */
+      if(newDate.externalapprove.length){
+        newDate.externalapprove = await Promise.all(newDate.externalapprove)
+      }
+      if(newDate.externalconform.length){
+        newDate.externalconform = await Promise.all(newDate.externalconform)
+      }
+
+      console.log('newDate: ', newDate);
+      return newDate;
+    } catch (error) {
+      console.error('Error transformDocumentApproval: ', error);
+      return error;
+    }
+  };
+
+  /**
+   * @param {object} value
+   * @param {array} value.externalapprove
+   * @param {array} value.externalconform
+   * @param {array} value.partners
+   * @param {string} value.name
+   * @param {string} value.customercode
+   * @param {string} value.okancode
+   * @param {string} value.okancode
+   * @return {Promise}
+   * @desc обновляем документ, тут собрано несколько последовательно выполняющийся методов
+   * */
+  updateDocument = async value => {
+    const {setNotificationError, setNotificationSuccess, history} = this.props;
+
+    const documentApproval = await this.transformDocumentApproval(value);
+    console.log('documentApproval:', documentApproval);
+
+    if(documentApproval.message){
+      setNotificationError(notificationOpts().error);
+      throw new SubmissionError({_error: documentApproval.message});
+    }
+
+    const options = {
+      variables: Object.assign({}, value, documentApproval),
       update: (store, response) => {
         try {
           const {
-            data: { updatedocument },
+            data: {updatedocument},
           } = response;
 
-          const data = store.readQuery({ query: DocumentItemQuery });
+          const data = store.readQuery({query: DocumentItemQuery});
 
           data.documentitem.push(updatedocument.document);
 
-          store.writeQuery({ query: DocumentItemQuery, data });
+          store.writeQuery({query: DocumentItemQuery, data});
         } catch (e) {
-          console.error('Error in FormProjectCreate, method submit : ', e);
+          console.error('Error in FormProjectCreate, method updateDocument : ', e);
         }
       },
     };
-    console.log('FormDocumentSettings', data);
+    console.log('updateDocument', options);
 
-    return this.props['@apollo/update'](data)
+    return this.props['@apollo/update'](options)
       .then(response => {
-        this.props.setNotificationSuccess(notificationOpts().success);
-        this.setState(() => ({ redirect: `/app/project/${data.variables.project}` }));
-
+        setNotificationSuccess(notificationOpts().success);
+        history.push(`/app/project/${options.variables.project}`);
         return response;
       })
-      .then(response => {
-        return sleep(1000);
-      })
-      .catch(({ graphQLErrors, message, networkError, ...rest }) => {
+      .catch(({graphQLErrors, message, networkError, ...rest}) => {
         console.log('graphQLErrors: ', graphQLErrors);
         console.log('message: ', message);
         console.log('networkError: ', networkError);
         console.log('rest: ', rest);
-        this.props.setNotificationError(notificationOpts().error);
-        if (graphQLErrors) {
-          throw new SubmissionError({ ...this.getNetworkError(graphQLErrors) });
-        } else {
-          throw new SubmissionError({ _error: message });
-        }
+        setNotificationError(notificationOpts().error);
+        throw new SubmissionError({_error: message});
       });
   };
 
@@ -111,53 +277,59 @@ export class FormDocumentSettings extends Component {
       handleSubmit,
       submitting,
       invalid,
-      initialValues: { project },
+      initialValues: {project},
     } = this.props;
 
-    const { redirect } = this.state;
-
-    if (redirect) {
-      return <Redirect to={redirect} />;
-    }
-
     return (
-      <Form onSubmit={handleSubmit(this.submit)}>
-        <Flex mt={9} justifyContent={'space-around'}>
-          <Box width={'55%'}>
+      <Form onSubmit={handleSubmit(this.updateDocument)}>
+        <Flex mt={9} mb={'200px'} justifyContent={'space-around'}>
+          <Box width={'50%'}>
             <Container maxWidth={'500px'} width={'100%'}>
-              <Query query={ProjectItemQuery} variables={{ id: project }}>
-                {({ loading, error, data }) => {
-                  console.log('FormDocumentSettings', data);
-                  if (loading) {
-                    return 'Загрузка...';
-                  }
-                  if (error) {
-                    return 'Произошла ошибка.';
-                  }
-                  if (!data || (data && !has.call(data, 'projectitem'))) {
-                    return null;
-                  }
-                  return (
-                    <Field
-                      component={SettingsUser}
-                      options={
-                        data.projectitem &&
-                        data.projectitem.partners.map(item => ({
-                          id: item.id,
-                          name: `${item.firstname} ${item.lastname} ${item.patronymic}`,
-                        }))
-                      }
-                      name={'partners'}
-                    />
-                  );
-                }}
-              </Query>
+              <InnerApprovalPartnersQuery
+                name={'partners'}
+                projectid={project}
+              />
             </Container>
 
             <Container maxWidth={'500px'} width={'100%'}>
               <Fields
-                names={['name', 'customercode', 'okancode']}
-                component={SettingsNameDocument}
+                names={['name', 'customercode', 'okancode', 'equipmentname']}
+                component={BasicDocumentSettings}
+              />
+            </Container>
+          </Box>
+          <Box width={'50%'}>
+
+            <Container mb={9} maxWidth={'500px'} width={'100%'}>
+              <Text
+                fontSize={6}
+                lineHeight={8}
+                color={'color7'}
+                textAlign={'center'}
+                mb={6}
+                fontFamily={'primary500'}>
+                Утверждающие документа
+              </Text>
+              <FieldArray
+                name="externalapprove"
+                component={ContractorListField}
+              />
+            </Container>
+
+            <Container maxWidth={'500px'} width={'100%'}>
+
+              <Text
+                fontSize={6}
+                lineHeight={8}
+                color={'color7'}
+                textAlign={'center'}
+                mb={6}
+                fontFamily={'primary500'}>
+                Внешние согласующие документа
+              </Text>
+              <FieldArray
+                name="externalconform"
+                component={ContractorListField}
               />
             </Container>
           </Box>
@@ -187,7 +359,7 @@ FormDocumentSettings = graphql(FormDocumentSettingsMutation, {
 
 FormDocumentSettings = connect(
   state => {
-    return { values: getFormValues('FormDocumentSettings')(state) };
+    return {values: getFormValues('FormDocumentSettings')(state)};
   },
   dispatch => ({
     setNotificationSuccess: message => dispatch(success(message)),
@@ -198,5 +370,5 @@ FormDocumentSettings = connect(
 FormDocumentSettings = reduxForm({
   form: 'FormDocumentSettings',
 })(FormDocumentSettings);
-
+FormDocumentSettings = withRouter(FormDocumentSettings)
 export default FormDocumentSettings;
--- src/modules/document/view/documentSettings/index.js
+++ src/modules/document/view/documentSettings/index.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Query } from 'react-apollo';
+import {connect} from 'react-redux';
+import {Query} from 'react-apollo';
 
 /** Redux user */
-import { getUserFromStore } from '../../../../store/reducers/user/selectors';
+import {getUserFromStore} from '../../../../store/reducers/user/selectors';
 
 /**PropTypes */
-import { ReactRoutePropTypes } from '../../../../propTypes/ReactRoutePropTypes';
+import {ReactRoutePropTypes} from '../../../../propTypes/ReactRoutePropTypes';
 
 /** View */
 import ErrorCatch from '@lib/ui/ErrorCatch/ErrorCatch';
@@ -19,23 +19,105 @@ import FormDocumentSettings from '../../component/FormDocumentSettings/FormDocum
 import DocumentItemQuery from './DocumentItemQuery.graphql';
 
 const has = Object.prototype.hasOwnProperty;
+const testData = {
+  documentitem: {
+    customercode: null,
+    equipmentname: null,
+    id: "5c76a94f9adb4973cf701b3d",
+    name: "afsfa",
+    okancode: null,
+    partners: [],
+    project: "5c76931c9adb4973cf701b29",
+    externalapprove: [
+      {
+        user: '5c76c63d335cf439ffd8148e',
+        approvaldate: '2019-03-01T00:59:08.468Z',
+      },
+      {
+        user: '5c76c63d335cf439ffd8148f',
+        approvaldate: '2019-02-01T00:59:08.468Z',
+      },
+      {
+        user: '5c76c63d335cf439ffd81490',
+        approvaldate: '2019-01-01T00:59:08.468Z',
+      },
+    ],
+    externalconform: [
+      {
+        user: '5c76c63d335cf439ffd81498',
+        approvaldate: '2019-02-22T00:59:08.468Z',
+      },
+      {
+        user: '5c76c63d335cf439ffd814a1',
+        approvaldate: '2019-02-22T00:59:08.468Z',
+      },
+    ],
+    __typename: "Document",
+  }
+};
 
 class DocumentSettingsPage extends Component {
-  static propTypes = { ...ReactRoutePropTypes };
+  static propTypes = {...ReactRoutePropTypes};
 
   state = {};
+
+  createContractorApprovalList = (approvallist, userlist) => {
+    try {
+      return approvallist.map(item => {
+        const indexUser = userlist.findIndex(user => user.id === item.user);
+        if (indexUser >= 0) {
+          return {
+            user: {
+              ...userlist[indexUser],
+              role: userlist[indexUser].role.name
+            },
+            approvaldate: item.approvaldate
+          }
+        }
+      })
+    } catch (error) {
+      console.error('Error createContractorApprovalList: ', error);
+      return error;
+    }
+  };
+
+  // TODO: в запрос userlist Добавить подпись, организацию и должность
+  // TODO: добавить описание методов
+  createInitialValues = ({documentitem, userlist}) => {
+    try {
+      const initialValues = {
+        ...documentitem,
+        partners: documentitem
+          ? documentitem.partners.map(item => item.id)
+          : [],
+      };
+
+      if (initialValues.externalapprove) {
+        initialValues.externalapprove = this.createContractorApprovalList(initialValues.externalapprove, userlist);
+      }
+      if (initialValues.externalconform) {
+        initialValues.externalconform = this.createContractorApprovalList(initialValues.externalconform, userlist);
+      }
+
+      return initialValues;
+    } catch (error) {
+      console.error('Error createInitialValues: ', error);
+      // TODO: вставить уведомление о ошибке инициализации формы
+      return {};
+    }
+  };
 
   render() {
     const {
       match: {
-        params: { id },
+        params: {id},
       },
     } = this.props;
 
     return (
       <ErrorCatch>
-        <Query query={DocumentItemQuery} variables={{ id: id }}>
-          {({ data, error, loading }) => {
+        <Query query={DocumentItemQuery} variables={{id: id}}>
+          {({data, error, loading}) => {
             // console.log('DocumentItemQuery', data);
 
             if (loading) {
@@ -53,12 +135,7 @@ class DocumentSettingsPage extends Component {
 
             return (
               <FormDocumentSettings
-                initialValues={{
-                  ...data.documentitem,
-                  partners: data.documentitem
-                    ? data.documentitem.partners.map(item => item.id)
-                    : [],
-                }}
+                initialValues={this.createInitialValues(data)}
               />
             );
           }}
