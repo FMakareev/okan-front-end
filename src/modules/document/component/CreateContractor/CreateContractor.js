e468284c464dfa089bc872cac250279598b62178
fmakareev <fmakareev@code-artel.com>
2019-03-04 15:26:41 +0300

    Стилизовал форму.

--- src/modules/document/component/CreateContractor/CreateContractor.js
+++ src/modules/document/component/CreateContractor/CreateContractor.js
@@ -1,31 +1,36 @@
 import React, {Component} from 'react';
 import {Field} from "redux-form";
 import {Box} from "@lib/ui/Box/Box";
+
 import required from "@lib/utils/validation/required";
 import TextFieldWithTooltip from "@lib/ui/TextFieldWithTooltip/TextFieldWithTooltip";
 import DayPickerField from "@lib/ui/DayPickerField/DayPickerField";
+import {TextFieldFirstWrapper} from "@lib/ui/TextFieldFirstWrapper/TextFieldFirstWrapper";
+import {TextFieldLastWrapper} from "@lib/ui/TextFieldLastWrapper/TextFieldLastWrapper";
 
 
 export class CreateContractor extends Component {
-  render(){
+  render() {
 
     const {names} = this.props;
     return (<Box>
-      <Field
-        name={names[0]}
-        component={TextFieldWithTooltip}
-        placeholder={'Наименование организации'}
-        type={"text"}
-        size={'md'}
-        fontFamily={'secondary'}
-        validate={required}
-      />
+      <TextFieldFirstWrapper>
+        <Field
+          name={names[0]}
+          component={TextFieldWithTooltip}
+          placeholder={'Наименование организации'}
+          type={"text"}
+          size={'sm'}
+          fontFamily={'secondary'}
+          validate={required}
+        />
+      </TextFieldFirstWrapper>
       <Field
         name={names[1]}
         component={TextFieldWithTooltip}
         placeholder={'Должность'}
         type={"text"}
-        size={'md'}
+        size={'sm'}
         fontFamily={'secondary'}
         validate={required}
       />
@@ -34,7 +39,7 @@ export class CreateContractor extends Component {
         component={TextFieldWithTooltip}
         placeholder={'Фамилия'}
         type={"text"}
-        size={'md'}
+        size={'sm'}
         fontFamily={'secondary'}
         validate={required}
       />
@@ -43,7 +48,7 @@ export class CreateContractor extends Component {
         component={TextFieldWithTooltip}
         placeholder={'Имя'}
         type={"text"}
-        size={'md'}
+        size={'sm'}
         fontFamily={'secondary'}
         validate={required}
       />
@@ -52,7 +57,7 @@ export class CreateContractor extends Component {
         component={TextFieldWithTooltip}
         placeholder={'Отчество'}
         type={"text"}
-        size={'md'}
+        size={'sm'}
         fontFamily={'secondary'}
         validate={required}
       />
@@ -61,19 +66,22 @@ export class CreateContractor extends Component {
         component={DayPickerField}
         placeholder={'Дата'}
         type={"text"}
-        size={'md'}
+        size={'sm'}
+        variant={'primary'}
         fontFamily={'secondary'}
         validate={required}
       />
-      <Field
-        name={names[6]}
-        component={TextFieldWithTooltip}
-        placeholder={'Подпись'}
-        type={"text"}
-        size={'md'}
-        fontFamily={'secondary'}
-        validate={required}
-      />
+      <TextFieldLastWrapper>
+        <Field
+          name={names[6]}
+          component={TextFieldWithTooltip}
+          placeholder={'Подпись'}
+          type={"text"}
+          size={'sm'}
+          fontFamily={'secondary'}
+          validate={required}
+        />
+      </TextFieldLastWrapper>
     </Box>)
   }
 }
