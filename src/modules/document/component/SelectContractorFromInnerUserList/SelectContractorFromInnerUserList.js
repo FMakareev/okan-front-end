3e1aa8f7879cd7ea70a1cae532fbb0acb84d1682
fmakareev <fmakareev@code-artel.com>
2019-03-04 15:29:15 +0300

    Удалил NewSelectStyles

--- src/modules/document/component/SelectContractorFromInnerUserList/SelectContractorFromInnerUserList.js
+++ src/modules/document/component/SelectContractorFromInnerUserList/SelectContractorFromInnerUserList.js
@@ -3,25 +3,6 @@ import {Query} from 'react-apollo';
 import {SelectBase, SelectStyles} from "@lib/ui/SelectBase/SelectBase";
 import UserListQuery from './UserListQuery.graphql';
 
-const NewSelectStyles = {
-  ...SelectStyles,
-  control: (style, props) => {
-    return {
-      ...style,
-      width: '100%',
-      padding: '0 0 0 10px',
-      cursor: 'pointer',
-      minHeight: '30px'
-    };
-  },
-  menu: style => {
-    return {
-      ...style,
-      margin: 0,
-      borderRadius: '5px',
-    };
-  },
-};
 
 
 export class SelectContractorFromInnerUserList extends Component {
@@ -36,7 +17,6 @@ export class SelectContractorFromInnerUserList extends Component {
           {...this.props}
           isLoading={loading}
           placeholder={'Выберите пользователя'}
-          styles={NewSelectStyles}
           labelKey={'name'}
           valueKey={'id'}
           options={
