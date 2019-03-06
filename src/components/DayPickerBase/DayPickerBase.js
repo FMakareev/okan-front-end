941c4d1e4ed4bc1a6f28e087bc9aae894a299949
fmakareev <fmakareev@code-artel.com>
2019-03-06 15:53:56 +0300

    Стилизовал прелоадер загрузки компонента. Сделал фабрику для создания инстансов DatePicker. Переименовал метод handleChange => onChangeDatePicker

--- src/components/DayPickerBase/DayPickerBase.js
+++ src/components/DayPickerBase/DayPickerBase.js
@@ -1,26 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {variant} from 'styled-system';
-import { ru } from 'date-fns/locale/ru';
-import dayjs from 'dayjs';
+import {ru} from 'date-fns/locale/ru';
 import styled from 'styled-components';
-import { asyncComponent } from 'react-async-component';
+import {asyncComponent} from 'react-async-component';
 
 /** PropTypes */
-import { fieldInputPropTypes } from '../../propTypes/Forms/FormPropTypes';
+import {fieldInputPropTypes} from '../../propTypes/Forms/FormPropTypes';
 
 /** Css */
 import '../../assets/style/react-datepicker-big.css';
 import {Box} from "@lib/ui/Box/Box";
+import {InputVariant} from "@lib/styles/variants/InputVariant";
+import {InputSize} from "@lib/styles/variants/InputSize";
 
-// const DatePicker = dynamic(import('react-datepicker'), {
-//   ssr: false,
-// });
-const DatePicker = asyncComponent({
-  resolve: () => import('react-datepicker'),
-  LoadingComponent: ({placeholder})=>(<Box border={'1px solid #848484'} px={'12px'} py={'10px'}>{placeholder}</Box>),
-  serverMode: 'defer'
-});
 
 const inputSize = variant({
   key: 'variant.inputSize',
@@ -30,10 +23,14 @@ const inputVariant = variant({
   key: 'variant.inputVariant',
   prop: 'variant',
 });
-const DatePickerStyled = styled(DatePicker)`
-  ${inputSize};
-  ${inputVariant};
-`;
+
+const DatePicker = () => asyncComponent({
+  resolve: () => import(/* webpackChunkName: 'chunk-name-react-datepicker'  */ 'react-datepicker'),
+  ErrorComponent: ({error}) => (<div>{error.message}</div>),
+  LoadingComponent: ({variant, size}) => (
+    <Box border={'1px solid #848484'} {...InputVariant[variant]} {...InputSize[size]}>Загрузка...</Box>),
+  serverMode: 'defer',
+});
 
 
 export class DayPickerBase extends Component {
@@ -44,7 +41,7 @@ export class DayPickerBase extends Component {
   };
 
   static defaultProps = {
-    input: { onChange: () => null, value: null },
+    input: {onChange: () => null, value: null},
     size: 'md',
     variant: 'default',
   };
@@ -52,35 +49,53 @@ export class DayPickerBase extends Component {
   constructor(props) {
     super(props);
     this.state = this.initialState;
-    this.handleChange = this.handleChange.bind(this);
+    this.onChangeDatePicker = this.onChangeDatePicker.bind(this);
+
+    /** @desc это костыль нужен для того чтобы асинхронная загрузка компонентов работала нормально, потому что при
+     * наличии двух одинаковых компонентов появившихся на стриницы +- в одно время рендерился только первый компонент,
+     * а второй зависал на загрузке и отвисал только после ручного перерендера.
+     * Работает так, для каждого инстанса компонента DayPickerBase мы создаем свой инстанс с асинхронной загрузкой датапикера
+     * благодаря этому для каждого инстанса DayPickerBase загрузка компонента происходит по отдельности
+     * */
+    this.DatePicker = DatePicker();
+    this.DatePickerStyled = styled(this.DatePicker)`
+      ${inputSize};
+      ${inputVariant};
+    `;
   }
 
   get initialState() {
-    const { input } = this.props;
+    const {input} = this.props;
 
-    return { startDate: input.value };
+    return {startDate: input.value};
   }
 
-  handleChange(date) {
-    const brithDay = dayjs(date).format('DD / MM / YYYY');
+  /**
+   * @param {string} date
+   * */
+  onChangeDatePicker(date) {
+    try {
+      console.log(date);
+      this.setState({startDate: new Date(date).toISOString()});
 
-    this.setState({ startDate: new Date(date).toISOString() });
+      const {
+        input: {onChange},
+      } = this.props;
 
-    const {
-      input: { onChange },
-    } = this.props;
-
-    onChange(new Date(date).toISOString());
+      onChange(new Date(date).toISOString());
+    } catch (error) {
+      console.error('Error : ', error);
+    }
   }
 
   render() {
-    const { placeholder, input } = this.props;
-    const { startDate } = this.state;
+    const {placeholder, input} = this.props;
+    const {startDate} = this.state;
 
     return (
-      <DatePickerStyled
+      <this.DatePickerStyled
         selected={startDate}
-        onChange={this.handleChange}
+        onChange={this.onChangeDatePicker}
         peekNextMonth
         showMonthDropdown
         showYearDropdown
@@ -90,7 +105,7 @@ export class DayPickerBase extends Component {
         input={input}
         locale={'ru'}
         popperPlacement="top-end"
-        popperModifiers={{ offset: { enabled: true, offset: '-130px, 0px' } }}
+        popperModifiers={{offset: {enabled: true, offset: '-130px, 0px'}}}
         {...this.props}
       />
     );
