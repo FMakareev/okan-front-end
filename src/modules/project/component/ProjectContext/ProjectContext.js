dfd6c51b019aae4ab0d5f15b108279637d4398c8
Plasteev Mikhail <m.plasteev@compaero.ru>
2019-03-13 14:44:04 +0300

    fixed

--- src/modules/project/component/ProjectContext/ProjectContext.js
+++ src/modules/project/component/ProjectContext/ProjectContext.js
@@ -1,5 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export const ProjectContext = React.createContext();
 
@@ -14,22 +14,18 @@ export const withProject = Component => {
 };
 
 /** @desc чтение и запись */
-export const PROJECT_MODE_RW = "PROJECT_MODE_WRITE-READ";
+export const PROJECT_MODE_RW = 'PROJECT_MODE_WRITE-READ';
 /** @desc чтение и комментирование*/
-export const PROJECT_MODE_RC = "PROJECT_MODE_READ_COMMENTING";
+export const PROJECT_MODE_RC = 'PROJECT_MODE_READ_COMMENTING';
 /** @desc только чтение */
-export const PROJECT_MODE_READ = "PROJECT_MODE_READ";
+export const PROJECT_MODE_READ = 'PROJECT_MODE_READ';
 /** @desc только чтение ревизии */
-export const PROJECT_MODE_REVISION = "PROJECT_MODE_REVISION";
+export const PROJECT_MODE_REVISION = 'PROJECT_MODE_REVISION';
 
 export const ProjectContextPropTypes = {
   project: PropTypes.shape({
     /** режим работы редактора */
-    mode: PropTypes.oneOf([
-      PROJECT_MODE_RW,
-      PROJECT_MODE_RC,
-      PROJECT_MODE_READ
-    ]),
+    mode: PropTypes.oneOf([PROJECT_MODE_RW, PROJECT_MODE_RC, PROJECT_MODE_READ]),
     /** редактируемый ли проект или нет, если нет то все кнопки отвечающие за редактирование скрываются */
     editable: PropTypes.bool,
     /** параметры роутера */
@@ -41,19 +37,19 @@ export const ProjectContextPropTypes = {
       /** id открытого проекта */
       projectid: PropTypes.string,
       /** id ревизии документа */
-      revisionid: PropTypes.string
+      revisionid: PropTypes.string,
     }),
     /** объект активного проекта */
     project: {
       author: PropTypes.shape({
-        id: PropTypes.string
+        id: PropTypes.string,
       }),
       createdate: PropTypes.string,
       documents: PropTypes.array,
       id: PropTypes.string,
       name: PropTypes.string,
       status: PropTypes.string,
-      updatedate: PropTypes.string
+      updatedate: PropTypes.string,
     }
-  })
+  }),
 };
