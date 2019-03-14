e016899770fd0fdcbecea2dc028c5cb159ea1454
fmakareev <fmakareev@code-artel.com>
2019-03-14 21:39:41 +0300

    Удалил методы componentDidMount,               componentWillReceiveProps,               getSectionNumber,               getSectionLetterNumber,               createPageTitle,               getMainSubsection. Заголовки вынес в компоненты. Вывод списка ячеек вынес в компонент. ContentWrapper вынес в компонент и назвал ProjectEditorContentWrapper

--- src/modules/project/component/ProjectEditor/ProjectEditor.js
+++ src/modules/project/component/ProjectEditor/ProjectEditor.js
@@ -1,22 +1,17 @@
 import PropTypes from 'prop-types';
 import React, {Component, Fragment} from 'react';
-import styled from 'styled-components';
 import {withRouter} from 'react-router-dom';
-import queryString from 'query-string';
 import {Query, withApollo} from 'react-apollo';
 
 /** Components */
-import EditorCellController from '../EditorCellController/EditorCellController';
 import {
   PROJECT_MODE_RW,
   ProjectContextPropTypes,
-  withProject,
 } from '../ProjectContext/ProjectContext';
 import {EditorAdditionalMenu} from '../EditorAdditionalMenu/EditorAdditionalMenu';
 
 /**View */
 import {Flex} from '@lib/ui/Flex/Flex';
-import {Box} from '@lib/ui/Box/Box';
 import {Text} from '@lib/ui/Text/Text';
 
 /** Graphql */
@@ -26,27 +21,37 @@ import CellItemQuery from '../DocumentTree/CellItemQuery.graphql';
 /** Utils */
 import {sortingCells} from '../../utils/sortingCells';
 
-/** Constants */
-import {BLOCK_TEXT} from '@lib/shared/blockType';
 
-/** COntext */
+/** Context */
 import {getPosition} from '../ProjectContext/ProjectContextSelectors';
 import ProjectModeState from '../ProjectContext/ProjectModeState';
+import {ProjectEditorNoSectionSelected} from "../ProjectEditorNoSectionSelected/ProjectEditorNoSectionSelected";
+import {ProjectEditorContentWrapper} from "../ProjectEditorContentWrapper/ProjectEditorContentWrapper";
+import ProjectEditorCellList from "../ProjectEditorCellList/ProjectEditorCellList";
 
-const ContentWrapper = styled.div`
-  background-color: #ffffff;
-  padding-top: 10px;
-  padding-bottom: 20px;
-  padding-left: 20px;
-  padding-right: 20px;
-  margin-bottom: 20px;
-  -webkit-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 5px;
-  min-height: 60px;
-`;
 
-const EditorCellControllerWithProject = withProject(props => <EditorCellController {...props} />);
+const ProjectEditorSecondTitle = ({children}) => (<Text
+  width={'100%'}
+  fontFamily={'secondary'}
+  lineHeight={8}
+  fontSize={6}
+  color={'color11'}
+  mt={'7px'}
+  ml={'5px'}
+  mb={'10px'}>
+  {children}
+</Text>);
+
+const ProjectEditorFirstTitle = ({children}) => (<Text
+  width={'100%'}
+  fontFamily={'secondary'}
+  lineHeight={8}
+  fontSize={6}
+  color={'color11'}
+  mt={'15px'}
+  ml={'15px'}>
+  {children}
+</Text>);
 
 
 export class ProjectEditor extends Component {
@@ -69,74 +74,6 @@ export class ProjectEditor extends Component {
     this.unsubscribeToCellItem();
   }
 
-  componentDidMount() {
-    if (this.props.sectionid) {
-      this.createPageTitle(getPosition(this.props.project, 'sectionid'));
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (
-      getPosition(nextProps.project, 'sectionid') !== getPosition(this.props.project, 'sectionid')
-    ) {
-      this.createPageTitle(getPosition(nextProps.project, 'sectionid'));
-    }
-  }
-
-  getSectionNumber = () => {
-    try {
-      return queryString.parse(this.props.location.search).sectionNumber;
-    } catch (e) {
-      console.error(e);
-      return '';
-    }
-  };
-  getSectionLetterNumber = () => {
-    try {
-      return queryString.parse(this.props.location.search).sectionLetterNumber;
-    } catch (e) {
-      console.error(e);
-      return '';
-    }
-  };
-
-  /**
-   * @param {string} id - id ячейки
-   * @desc метод для создания заголовков
-   * */
-  createPageTitle = async id => {
-    try {
-      this.unsubscribeToCellItem();
-
-      let childCell = await this.getCellItem(id);
-      this.subscribeInstanceToUpdateTitle = this.subscribeToCellItem(
-        childCell.data.cellitem.id,
-      ).subscribe(({data}) => {
-        const name = data.cellitem && data.cellitem.name;
-        return this.setState(state => ({
-          ...state,
-          childName: name,
-          parentNumber: this.getSectionNumber(),
-          parentLetterNumber: this.getSectionLetterNumber(),
-        }));
-      });
-
-      if (childCell && childCell.data.cellitem.parent) {
-        let parentCell = await this.getCellItem(childCell.data.cellitem.parent.id);
-        this.subscribeInstanceToUpdateParentTitle = this.subscribeToCellItem(
-          parentCell.data.cellitem.id,
-        ).subscribe(({data}) => {
-          const name = data.cellitem && data.cellitem.name;
-          return this.setState(state => ({
-            ...state,
-            parentName: name,
-          }));
-        });
-      }
-    } catch (error) {
-      console.log('Error createPageTitle: ', error);
-    }
-  };
 
   /** @desc сбрасываем подписки на изменение ячеек */
   unsubscribeToCellItem = () => {
@@ -170,41 +107,43 @@ export class ProjectEditor extends Component {
    * @desc метод выполняет получение данные по ячейке
    * */
   getCellItem = id => {
-    return this.props.client.query({ query: CellItemQuery, variables: { id } }).catch(error => {
+    return this.props.client.query({query: CellItemQuery, variables: {id}}).catch(error => {
       console.log('Error getCellItem: ', error);
     });
   };
 
   /**
-   * @param {string} parentNumber - Номер подраздела
-   * @param {string} parentLetterNumber - Буквенный номер приложения
-   * @param {string} childName - Имя ячейки подраздела
-   * @param {string} section - Номер раздела
-   * @param {string} parentName - Имя ячейки раздела
    * @desc метод для получения заголовков
    * */
-  getMainSection = (parentNumber, parentLetterNumber, childName, section, parentName) => {
-    return parentNumber && parentNumber.length === 2 ? (
-      <Fragment>{`${parentNumber || parentLetterNumber} ${childName || ''}`}</Fragment>
-    ) : (
-      <Fragment>{`${section} ${!parentLetterNumber ? parentName : ''}`}</Fragment>
-    );
+  getSectionTitle = (cellitem) => {
+    if(!cellitem){
+      return {};
+    }
+    if (cellitem.isAttachment) {
+      return {
+        firstTitle: `Приложение ${cellitem.number.join('.').toUpperCase()}. ${cellitem.name}`,
+        secondTitle: null,
+        sectionNumber: `${cellitem.number.join('.').toUpperCase()}.`,
+      }
+    } else if (!cellitem.isAttachment) {
+      if (cellitem.parent) {
+        return {
+          firstTitle: `${cellitem.parent.number.join('.')}. ${cellitem.parent.name}`,
+          secondTitle: `${cellitem.number.join('.')}. ${cellitem.name}`,
+          sectionNumber: `${cellitem.number.join('.')}.`,
+        }
+      } else {
+        return {
+          firstTitle: `${cellitem.number.join('.')}. ${cellitem.name}`,
+          secondTitle: null,
+          sectionNumber: `${cellitem.number.join('.')}.`,
+        }
+      }
+    }
+    return {};
   };
 
-  /**
-   * @param {string} parentNumber - Номер подраздела
-   * @param {string} parentLetterNumber - Буквенный номер приложения
-   * @param {string} childName - Имя ячейки подраздела
-   * @desc метод для получения подзаголовков
-   * */
-  getMainSubsection = (parentNumber, parentLetterNumber, childName) => {
-    return parentNumber && parentNumber.length <= 2 ? null : (
-      <Fragment>{`${parentNumber ||
-        (parentLetterNumber && parentLetterNumber
-          ? `Приложение ${parentLetterNumber} `
-          : null)} ${childName || ''}`}</Fragment>
-    );
-  };
+
 
   render() {
     const {
@@ -212,27 +151,20 @@ export class ProjectEditor extends Component {
       project,
     } = this.props;
 
-    const {childName, parentName, parentNumber, parentLetterNumber} = this.state;
+    const {parentLetterNumber} = this.state;
+    const sectionid = getPosition(project, 'sectionid');
 
-
-    if (!getPosition(project, 'sectionid')) {
-      return (
-        <Flex pl={'10px'} pr={'40px'} mb={'20px'} pt={'60px'} flexDirection={'column'}>
-          <ContentWrapper>
-            <Text fontFamily={'primary300'} fontSize={6} lineHeight={8}>
-              Раздел не выбран.
-            </Text>
-          </ContentWrapper>
-        </Flex>
-      );
+    if (!sectionid) {
+      return (<ProjectEditorNoSectionSelected/>);
     }
+
     return (
       <Flex pl={'10px'} pr={'40px'} mb={'20px'} pt={'5px'} flexDirection={'column'}>
         <Query
-          skip={!getPosition(project, 'sectionid')}
+          skip={!sectionid}
           query={CellListQuery}
-          variables={{ parent: getPosition(project, 'sectionid') }}>
-          {({ data, loading, error }) => {
+          variables={{parent: sectionid}}>
+          {({data, loading, error}) => {
             if (loading) {
               return `Загрузка`;
             }
@@ -240,95 +172,48 @@ export class ProjectEditor extends Component {
             if (error) {
               return null;
             }
+            console.log('data.cellitem: ', data.cellitem);
 
+            const {
+              firstTitle,
+              secondTitle,
+              sectionNumber
+            } = this.getSectionTitle(data.cellitem);
             if (data && data.celllist) {
-              const section = parentNumber ? parentNumber.slice(0, -2) : '';
 
-              // const sectionMain =
-              //   data.celllist.length !== 0
-              //     ? data.celllist &&
-              //       data.celllist[0].parent &&
-              //       data.celllist[0].parent.parent &&
-              //       data.celllist[0].parent.parent.number.join('.')
-              //     : '';
-
-              const sectionSubsection =
-                data.celllist.length !== 0
-                  ? data.celllist &&
-                    data.celllist[0].parent &&
-                    data.celllist[0].parent.number.join('.').toUpperCase()
-                  : parentNumber;
-
-              let childCellIndex = 0;
               return (
                 <Fragment>
-                  <Text
-                    width={'100%'}
-                    fontFamily={'secondary'}
-                    lineHeight={8}
-                    fontSize={6}
-                    color={'color11'}
-                    mt={'15px'}
-                    ml={'15px'}>
-                    {this.getMainSection(
-                      parentNumber,
-                      parentLetterNumber,
-                      childName,
-                      section,
-                      parentName,
-                    )}
-                  </Text>
-                  <ContentWrapper>
-                    <Text
-                      width={'100%'}
-                      fontFamily={'secondary'}
-                      lineHeight={8}
-                      fontSize={6}
-                      color={'color11'}
-                      mt={'7px'}
-                      ml={'5px'}
-                      mb={'10px'}>
-                      {this.getMainSubsection(parentNumber, parentLetterNumber, childName)}
-                    </Text>
+                  {
+                    firstTitle &&
+                    <ProjectEditorFirstTitle>
+                      {firstTitle}
+                    </ProjectEditorFirstTitle>
+                  }
+                  <ProjectEditorContentWrapper>
+                    {
+                      secondTitle &&
+                      <ProjectEditorSecondTitle>
+                        {secondTitle}
+                      </ProjectEditorSecondTitle>
+                    }
+                    <ProjectEditorCellList
+                      childCellIndex={0}
+                      parentNumber={sectionNumber}
+                      parentLetterNumber={parentLetterNumber}
+                      celllist={sortingCells(data.celllist)}
+                    />
+                  </ProjectEditorContentWrapper>
+                </Fragment>
+              );
+            }
 
-                      {
-                        sortingCells(data.celllist).map((item, index) => {
-                          if (item.content && item.content.contenttype === BLOCK_TEXT) {
-                            childCellIndex += 1;
-                          }
-
-                          return (
-                            <Box
-                              key={`EditorCellControllerWithProject-${index}`}
-                              pb={6}
-                              pt={6}
-                              position={'relative'}
-                              zIndex={data.celllist.length - index}
-                            >
-                              <EditorCellControllerWithProject
-                                key={`EditorCellControllerWithProject-${index}`}
-                                data={item}
-                                editable={
-                                  item.content.parentNumber === 0 // редактирование первого блока и не запускает автосохранение // TODO: эта штука работает не так, проблема в том что она каждый раз включает
-                                }
-                                sectionNumber={`${sectionSubsection}.${childCellIndex}`}
-                                parentLetterNumber={parentLetterNumber}
-                              />
-                            </Box>);
-                        })
-                      }
-                    </ContentWrapper>
-                  </Fragment>
-                );
-              }
-
-              return null;
-            }}
-          </Query>
-          <ProjectModeState is={PROJECT_MODE_RW}>
-            <EditorAdditionalMenu parentid={getPosition(project, 'sectionid')} activeMenu/>
-          </ProjectModeState>
-        </Flex>
+            return null;
+          }}
+        </Query>
+        <ProjectModeState is={PROJECT_MODE_RW}>
+          <EditorAdditionalMenu parentid={getPosition(project, 'sectionid')} activeMenu/>
+        </ProjectModeState>
+      </Flex>
     );
   }
 }
