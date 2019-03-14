7affabeafd8bd2154887f23ba7a60034209f2f19
fmakareev <fmakareev@code-artel.com>
2019-03-14 10:07:20 +0300

    Метод getLocationSearchPhrase вынес из класса. Добавил свойство documentitem. Получение списка документов вынес в селектор. Убрал проверку на режим проекта из SidebarDocumentSearchWithProject

--- src/modules/project/component/ProjectSidebar/ProjectSidebar.js
+++ src/modules/project/component/ProjectSidebar/ProjectSidebar.js
@@ -11,6 +11,8 @@ import ProjectModeState from '../ProjectContext/ProjectModeState';
 import {SidebarDocumentSearch} from "../SidebarDocumentSearch/SidebarDocumentSearch";
 import queryString from "query-string";
 import {withRouter} from "react-router-dom";
+import {getLocationSearchPhrase} from "../../utils/getLocationSearchPhrase";
+import {getProject} from "../ProjectContext/ProjectContextSelectors";
 
 const DocumentTreeWithProject = withProject(props => <DocumentTree {...props} />);
 const FormCreateDocumentWithProject = withProject(props => <FormCreateDocument {...props} />);
@@ -25,37 +27,36 @@ export class ProjectSidebar extends Component {
     })
   }
 
-  getLocationSearchPhrase = (search) => {
-    try{
-      return queryString.parse(search).searchPhrase
-    } catch(error){
-      console.error('Error: ', error);
-      return null;
-    }
-  };
+  static defaultProps = {
+    project: {},
+  }
+
 
   render() {
-    const {location, project: {project: {documents, id}}} = this.props;
+    const {location, documentitem, project} = this.props;
+    const documents = getProject(project, 'documents');
     return (
       <Fragment>
-        {documents &&
-        documents.map(item => (
-          <DocumentTreeWithProject data={item} key={`DocumentTree=${item.id}`}/>
-        ))}
+        {
+          !documentitem && documents &&
+          documents.map(item => (
+            <DocumentTreeWithProject data={item} key={`DocumentTree=${item.id}`}/>
+          ))
+        }
 
-        {!documents && <DocumentTreeWithProject data={this.props} key={`DocumentTree=${id}`}/>}
+        {
+          documentitem && <DocumentTreeWithProject data={documentitem}/>
+        }
 
         <ProjectModeState is={PROJECT_MODE_RW}>
           <FormCreateDocumentWithProject/>
         </ProjectModeState>
 
-        <ProjectModeState is={PROJECT_MODE_RW}>
-          <SidebarDocumentSearchWithProject
-            initialValues={{
-              name: this.getLocationSearchPhrase(location.search),
-            }}
-          />
-        </ProjectModeState>
+        <SidebarDocumentSearchWithProject
+          initialValues={{
+            name: getLocationSearchPhrase(location.search),
+          }}
+        />
       </Fragment>
     );
   }
