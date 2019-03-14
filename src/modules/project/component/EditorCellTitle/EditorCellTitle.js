c1a4aad28c593891075799ad3de7818d87112be4
fmakareev <fmakareev@code-artel.com>
2019-03-14 21:35:21 +0300

    Форматирование.

--- src/modules/project/component/EditorCellTitle/EditorCellTitle.js
+++ src/modules/project/component/EditorCellTitle/EditorCellTitle.js
@@ -3,10 +3,10 @@ import React from 'react';
 import styled from 'styled-components';
 import ReactHTMLParser from 'react-html-parser';
 
-import { Text } from '@lib/ui/Text/Text';
-import { Flex } from '@lib/ui/Flex/Flex';
-import { Box } from '@lib/ui/Box/Box';
-import { BLOCK_IMAGE, BLOCK_TABLE } from '@lib/shared/blockType';
+import {Text} from '@lib/ui/Text/Text';
+import {Flex} from '@lib/ui/Flex/Flex';
+import {Box} from '@lib/ui/Box/Box';
+import {BLOCK_IMAGE, BLOCK_TABLE} from '@lib/shared/blockType';
 
 const NameStyled = styled(Box)`
   p,
@@ -19,19 +19,17 @@ const NameStyled = styled(Box)`
 `;
 // TODO рефакторинг: с 32 по 43 строки оптимизировать
 export const EditorCellTitle = ({
-  content,
-  editable,
-  onClick,
-  contenttype,
-  parentLetterNumber,
-  textAlign = 'center',
-}) => {
+                                  content,
+                                  editable,
+                                  onClick,
+                                  contenttype,
+                                  parentLetterNumber,
+                                  textAlign = 'center',
+                                }) => {
   if (content.contenttype !== contenttype || editable) return null;
   return (
     <Text
-      onClick={() => {
-        onClick();
-      }}
+      onClick={onClick}
       fontWeight={'bold'}
       fontSize={6}
       color={'color11'}
@@ -53,9 +51,11 @@ export const EditorCellTitle = ({
           </Box>
         )}
         <NameStyled>
-          {content.name &&
+          {
+            content.name &&
             typeof content.name === 'string' &&
-            ReactHTMLParser(content.name.replace('data-f-id="pbf"', 'style="display:none;"'))}
+            ReactHTMLParser(content.name.replace('data-f-id="pbf"', 'style="display:none;"'))
+          }
         </NameStyled>
       </Flex>
     </Text>
