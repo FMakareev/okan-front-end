d55cab9960396294ad06f054010656ab96a0a628
fmakareev <fmakareev@code-artel.com>
2019-03-14 10:04:48 +0300

    Создал компонент ProjectEditorWrapper

---
+++ src/modules/project/component/ProjectEditorWrapper/ProjectEditorWrapper.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ProjectEditorWrapper = styled.div`
+  margin-left: 340px;
+  width: calc(100% - 380px);
+  min-height: calc(100vh - 40px);
+`;
