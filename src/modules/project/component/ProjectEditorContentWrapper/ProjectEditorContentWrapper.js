388eba48c10b091ba94d3075fb307eb4b6c6952a
fmakareev <fmakareev@code-artel.com>
2019-03-14 20:55:48 +0300

    Создал компонент ContentWrapper

---
+++ src/modules/project/component/ProjectEditorContentWrapper/ProjectEditorContentWrapper.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from "styled-components";
+
+
+export const ProjectEditorContentWrapper = styled.div`
+  background-color: #ffffff;
+  padding-top: 10px;
+  padding-bottom: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-bottom: 20px;
+  -webkit-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+  min-height: 60px;
+`;
+
+export default ProjectEditorContentWrapper;
