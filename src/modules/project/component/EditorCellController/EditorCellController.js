5c3c0cdbf3aecca54bdc70dda15c3128ed066db0
fmakareev <fmakareev@code-artel.com>
2019-03-18 20:18:03 +0300

    sync

--- src/modules/project/component/EditorCellController/EditorCellController.js
+++ src/modules/project/component/EditorCellController/EditorCellController.js
@@ -172,7 +172,7 @@ export class EditorCellController extends Component {
    * @desc Метод для сохранения ячейки
    * */
   saveCellContent() {
-    const content = typeof this.props.values.content ? this.props.values.content.replace(FROALA_WATER_MARK) : this.props.values.content;
+    const content = typeof this.props.values.content ? this.props.values.content.replace(FROALA_WATER_MARK) : '';
     return this.props
       .mutate({
         variables: {
