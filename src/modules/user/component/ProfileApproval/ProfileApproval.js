add6828d23a19f168065a1659528db79961183e1
fmakareev <fmakareev@code-artel.com>
2019-03-22 12:27:20 +0300

    Поправил Стили.

--- src/modules/user/component/ProfileApproval/ProfileApproval.js
+++ src/modules/user/component/ProfileApproval/ProfileApproval.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, {Fragment} from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
@@ -6,14 +6,14 @@ import PropTypes from 'prop-types';
 import Text from '@lib/ui/Text/Text';
 import Flex from '@lib/ui/Flex/Flex';
 import Tbody from '@lib/ui/Table/Tbody';
-import Td from '@lib/ui/Table/Td';
-import Tr from '@lib/ui/Table/Tr';
+
 import Link from '@lib/ui/Link/Link';
 
 /** Constants */
-import { TO_APPROVAL } from '../../../../shared/approvalStatus';
+import {TO_APPROVAL} from '../../../../shared/approvalStatus';
+import {Box} from "@lib/ui/Box/Box";
 
-const TrStyled = styled(Tr)`
+const TrStyled = styled(Box)`
   display: -webkit-box;
   display: -ms-flexbox;
   display: flex;
@@ -27,7 +27,7 @@ const TrStyled = styled(Tr)`
   }
 `;
 
-const TbodyStyled = styled(Tbody)`
+const TbodyStyled = styled(Box)`
   border: 1px solid #848484;
   border-radius: 5px;
   margin: 10px 0 0 0;
@@ -43,14 +43,18 @@ const LinkStyled = styled(Link)`
   justify-content: space-between;
 `;
 
-const TdStyle = styled(Td)`
-  word-wrap: break-word;
+const TdStyle = styled(Box)`
   width: 50%;
   padding: 0 5px;
   text-align: center;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  color: #4F4F4F;
 `;
 
-export const ProfileApproval = ({ data }) => {
+export const ProfileApproval = ({data}) => {
+  console.log('ProfileApproval: ', data);
   return (
     <Fragment>
       <Text
@@ -63,42 +67,42 @@ export const ProfileApproval = ({ data }) => {
         На согласование
       </Text>
       <Tbody>
-        <Flex justifyContent={'space-around'}>
-          <Text fontSize={6} lineHeight={8} fontFamily={'primary500'}>
-            Номер документа
-          </Text>
+      <Flex justifyContent={'space-around'}>
+        <Text fontSize={6} lineHeight={8} fontFamily={'primary500'}>
+          Номер документа
+        </Text>
 
-          <Text fontSize={6} lineHeight={8} fontFamily={'primary500'}>
-            Название документа
-          </Text>
-        </Flex>
+        <Text fontSize={6} lineHeight={8} fontFamily={'primary500'}>
+          Название документа
+        </Text>
+      </Flex>
       </Tbody>
       {Array.isArray(data) &&
-        data.map(
-          item =>
-            item.approvalstatus === TO_APPROVAL && (
-              <LinkStyled to={`/app/document-commenting/${item.project}/${item.id}`}>
-                <TbodyStyled>
-                  <TrStyled>
-                    <TdStyle fontFamily={'primary500'}>{item.okancode}</TdStyle>
-                    <TdStyle fontFamily={'secondaryBold'}>{item.name}</TdStyle>
-                  </TrStyled>
-                </TbodyStyled>
-              </LinkStyled>
-            ),
-        )}
+      data.map(
+        item =>
+          item.approvalstatus === TO_APPROVAL && (
+            <LinkStyled key={`ProfileApproval-${item.id}`} to={`/app/document-commenting/${item.project}/${item.id}`}>
+              <TbodyStyled>
+                <TrStyled>
+                  <TdStyle title={item.okancode} fontFamily={'primary500'}>{item.okancode}</TdStyle>
+                  <TdStyle title={item.name} fontFamily={'secondaryBold'}>{item.name}</TdStyle>
+                </TrStyled>
+              </TbodyStyled>
+            </LinkStyled>
+          ),
+      )}
     </Fragment>
   );
 };
 
 ProfileApproval.propTypes = {
-  data: PropTypes.arrayOf({
+  data: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.string,
     /** name document */
     name: PropTypes.string,
     /** number document*/
     documentnumber: PropTypes.string,
-  }),
+  })),
 };
 
 export default ProfileApproval;
