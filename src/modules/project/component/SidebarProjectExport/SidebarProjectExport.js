d8d0445464c958ebf8105735034a1951c2f9b176
MICHAel <plasteefff@gmail.com>
2019-05-13 15:03:12 +0300

    Fixed export document

--- src/modules/project/component/SidebarProjectExport/SidebarProjectExport.js
+++ src/modules/project/component/SidebarProjectExport/SidebarProjectExport.js
@@ -2,8 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { error, success } from 'react-notification-system-redux';
-import FileSaver from 'file-saver';
-import { captureException } from '../../../../hocs/withSentry/withSentry';
+import { exportDocument } from '../../../../hocs/withExportDocument/withExportDocument';
 
 /** View */
 import ButtonBase from '../../../../components/ButtonBase/ButtonBase';
@@ -22,33 +21,9 @@ export class SidebarProjectExport extends React.Component {
   };
   static defaultProps = {};
 
-  exportDocument = () => {
-    const {
-      document: { name, id },
-    } = this.props;
-
-    let formData = new FormData();
-    formData.append('document', id);
-    return fetch(`${ENDPOINT_CLIENT}/docx_converter/convert`, {
-      method: 'POST',
-      credentials: 'include',
-      mode: 'no-cors',
-      body: formData,
-    })
-      .then(response => {
-        return response.blob();
-      })
-      .then(blob => {
-        FileSaver.saveAs(blob, `${name || 'document'}.docx`);
-      })
-      .catch(error => {
-        captureException(error, 'Error exportDocument: ');
-      });
-  };
-
   render() {
     const {
-      document: { id },
+      document: { id, name },
     } = this.props;
     console.log('SidebarProjectExport: ', this.props);
     return (
@@ -56,7 +31,7 @@ export class SidebarProjectExport extends React.Component {
         title={'Эспортировать документ'}
         onClick={event => {
           event.stopPropagation();
-          this.exportDocument(id);
+          exportDocument(id, name);
         }}
         variant={'outlineGray'}
         p={'2px'}
