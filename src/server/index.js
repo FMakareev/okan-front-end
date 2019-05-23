10f4734e068268f6c49d3df1ba7a8e29dc2071bf
fmakareev <fmakareev@code-artel.com>
2019-05-23 14:17:06 +0300

    change query

--- src/server/index.js
+++ src/server/index.js
@@ -13,59 +13,55 @@ const jsonTranslate = require('../store/reducers/localization/localization.json'
 const langArray = jsonTranslate.map(item => item.code);
 const has = Object.prototype.hasOwnProperty;
 
-try{
-  app.use(express.static('public'));
-  app.use(cookieParser());
+app.use(express.static('public'));
+app.use(cookieParser());
 
-  if (langArray.length) {
-    app.use(
-      requestLanguage({
-        languages: langArray,
-        queryName: 'lang',
-        cookie: {
-          name: 'lang',
-          options: {
-            path: '/',
-            maxAge: 3650 * 24 * 3600 * 1000, // 10 years in miliseconds
-          },
-          url: '/lang/{language}',
+if (langArray.length) {
+  app.use(
+    requestLanguage({
+      languages: langArray,
+      queryName: 'lang',
+      cookie: {
+        name: 'lang',
+        options: {
+          path: '/',
+          maxAge: 3650 * 24 * 3600 * 1000, // 10 years in miliseconds
         },
-      }),
-    );
+        url: '/lang/{language}',
+      },
+    }),
+  );
+}
+
+
+Object.entries(modules).map(([moduleName, value]) => {
+  if (has.call(value, 'routes')) {
+    value.routes.forEach(item => {
+      try{
+        app[item.method](item.path, item.callback);
+      } catch(error){
+        console.error(`Error ${moduleName}: `,error);
+      }
+    });
+    return null;
   }
-
-
-  Object.entries(modules).map(([moduleName, value]) => {
-    if (has.call(value, 'routes')) {
-      value.routes.forEach(item => {
-        try{
-          app[item.method](item.path, item.callback);
-        } catch(error){
-          console.error(`Error ${moduleName}: `,error);
-        }
-      });
-      return null;
-    }
-    console.error(
-      `ERROR:in the module "${moduleName}" there is no property "routes".
+  console.error(
+    `ERROR:in the module "${moduleName}" there is no property "routes".
         Add the property "routes" to the module "${moduleName}" and determine at least
         one route otherwise the module will be inaccessible to users.`,
-    );
-    return null;
-  });
+  );
+  return null;
+});
 
-  /**
-   * @description http://expressjs.com/en/4x/api.html#app.get.method
-   * */
-  app.use('*', Root);
+/**
+ * @description http://expressjs.com/en/4x/api.html#app.get.method
+ * */
+app.use('*', Root);
 
-  app.listen(PORT, () => {
-    console.log(`Server is listening on port:${PORT}. !!!!!!!!`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server is listening on port:${PORT}. !!!!!!!!`);
+});
 
-} catch(error){
-  console.error('Error server: ',error);
-}
 //
 // Hot Module Replacement
 // -----------------------------------------------------------------------------
