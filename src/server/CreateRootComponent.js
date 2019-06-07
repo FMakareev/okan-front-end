d14c728933fd2c238a0e0e91316a93d7875f7d71
fmakareev <fmakareev@code-artel.com>
2019-06-07 12:40:26 +0300

    change order provider

--- src/server/CreateRootComponent.js
+++ src/server/CreateRootComponent.js
@@ -1,15 +1,15 @@
 import React, {Fragment} from 'react';
 import url from 'url';
-import { AsyncComponentProvider } from 'react-async-component';
-import { StaticRouter } from 'react-router';
-import { renderRoutes } from 'react-router-config';
-import { StyleSheetManager } from 'styled-components';
+import {AsyncComponentProvider} from 'react-async-component';
+import {StaticRouter} from 'react-router';
+import {renderRoutes} from 'react-router-config';
+import {StyleSheetManager} from 'styled-components';
 
-import { Provider as ProviderRedux } from 'react-redux';
-import { ApolloProvider } from 'react-apollo';
+import {Provider as ProviderRedux} from 'react-redux';
+import {ApolloProvider} from 'react-apollo';
 import StyledThemeProvider from '../styles/StyledThemeProvider';
-import { ConfigRouter } from '../routes';
-import { GlobalStyle } from "../styles/GlobalStyle";
+import {ConfigRouter} from '../routes';
+import {GlobalStyle} from "../styles/GlobalStyle";
 
 /**
  * @params {} asyncContext -
@@ -22,39 +22,41 @@ import { GlobalStyle } from "../styles/GlobalStyle";
  * @description root react component
  * */
 export const CreateRootComponent = ({
-  asyncContext,
-  STStyleRenderer,
-  request,
-  Store,
-  RouterContext,
-  ApolloClient,
-}) => {
+                                      asyncContext,
+                                      STStyleRenderer,
+                                      request,
+                                      Store,
+                                      RouterContext,
+                                      ApolloClient,
+                                    }) => {
   /**
    * @description parse request url
    * */
-  const { search } = url.parse(request.originalUrl);
+  const {search} = url.parse(request.originalUrl);
 
   return (
     <AsyncComponentProvider asyncContext={asyncContext}>
-      <ProviderRedux store={Store}>
-        <ApolloProvider client={ApolloClient}>
-          <StyleSheetManager sheet={STStyleRenderer.instance}>
-            <StyledThemeProvider>
-              <Fragment>
-                <GlobalStyle/>
-                <StaticRouter
-                  location={{
-                    pathname: request.originalUrl,
-                    search,
-                  }}
-                  context={RouterContext}>
-                  {renderRoutes(ConfigRouter)}
-                </StaticRouter>
-              </Fragment>
-            </StyledThemeProvider>
-          </StyleSheetManager>
-        </ApolloProvider>
-      </ProviderRedux>
+
+      <StyleSheetManager sheet={STStyleRenderer.instance}>
+        <StyledThemeProvider>
+          <ApolloProvider client={ApolloClient}>
+              <ProviderRedux store={Store}>
+                <Fragment>
+                  <GlobalStyle/>
+                  <StaticRouter
+                    location={{
+                      pathname: request.originalUrl,
+                      search,
+                    }}
+                    context={RouterContext}>
+                    {renderRoutes(ConfigRouter)}
+                  </StaticRouter>
+                </Fragment>
+              </ProviderRedux>
+          </ApolloProvider>
+        </StyledThemeProvider>
+      </StyleSheetManager>
+
     </AsyncComponentProvider>
   );
 };
