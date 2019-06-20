fdd8ad32d11866164c2e8adcbd058725ea248bb5
fmakareev <fmakareev@code-artel.com>
2019-06-20 21:09:58 +0300

    Пофиксил критический баг

--- src/components/FroalaReduxForm/DefineIcons.js
+++ src/components/FroalaReduxForm/DefineIcons.js
@@ -1,38 +1,41 @@
 import $ from 'jquery';
-import Froalaeditor from 'froala-editor';
 import copyIcon from '../../assets/image/copyIcon.png';
 import bindIcon from '../../assets/image/bindIcon.png';
 import deleteIcon from '../../assets/image/deleteIcon.png';
 
 export function DefineIcons(props) {
-  Froalaeditor.DefineIcon('copy', {
-    SRC: copyIcon,
-    ALT: 'Copy',
-    template: 'image',
-  });
-  Froalaeditor.RegisterCommand('copy', {
-    title: 'Copy block',
-    focus: true,
-    undo: false,
-  });
+  try {
+    $.FroalaEditor.DefineIcon('copy', {
+      SRC: copyIcon,
+      ALT: 'Copy',
+      template: 'image',
+    });
+    $.FroalaEditor.RegisterCommand('copy', {
+      title: 'Copy block',
+      focus: true,
+      undo: false,
+    });
 
-  Froalaeditor.DefineIcon('bind', { SRC: bindIcon, ALT: 'Bind', template: 'image' });
-  Froalaeditor.RegisterCommand('bind', {
-    title: 'Bind block',
-    focus: true,
-    undo: false,
-  });
+    $.FroalaEditor.DefineIcon('bind', {SRC: bindIcon, ALT: 'Bind', template: 'image'});
+    $.FroalaEditor.RegisterCommand('bind', {
+      title: 'Bind block',
+      focus: true,
+      undo: false,
+    });
 
-  Froalaeditor.DefineIcon('unbind', { SRC: deleteIcon, ALT: 'unbind', template: 'image' });
-  Froalaeditor.RegisterCommand('unbind', {
-    title: 'Unbind block',
-    focus: true,
-    undo: false,
-    refreshAfterCallback: false,
-    callback: command => {
-      props.buttonClick(command);
-    },
-  });
+    $.FroalaEditor.DefineIcon('unbind', {SRC: deleteIcon, ALT: 'unbind', template: 'image'});
+    $.FroalaEditor.RegisterCommand('unbind', {
+      title: 'Unbind block',
+      focus: true,
+      undo: false,
+      refreshAfterCallback: false,
+      callback: command => {
+        props.buttonClick(command);
+      },
+    });
+  } catch (e) {
+    console.log('DefineIcons: ', e);
+  }
 
   return null;
 }
